e187b3c43f9ba1ea3adf1e14304fbad31433d1bd
Sean Bolak <seanbolak@gmail.com>
2017-05-08 15:39:16 -0400

    updated some deps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { LOCATION_CHANGE, syncHistoryWithStore, routerMiddleware, routerActions } from 'react-router-redux';
+import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render(
   <App />,
