5a3c4fde79d17478a7b22582106330fcb1f8f706
Christopher Best <bestchris@gmail.com>
2017-05-19 12:07:08 -0400

    add dropdown for project status

--- src/views/ProjectManagement/components/ProjectCard.js
+++ src/views/ProjectManagement/components/ProjectCard.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const ProjectStatusOptions = [
+  {
+    label: 'Deactivate',
+    value: 'deactivate'
+  },
+  {
+    label: 'Activate',
+    value: 'activate'
+  }
+];
+
+class CardValueDropdown extends Component {
+  render() {
+    return (
+      <div className='card-value card-value-select' tabIndex="0">
+        {this.props.value}
+        <span className='card-value-select-arrow'></span>
+        <div className='card-value-options'>
+          {this.props.options.map(option =>
+            <div
+              className='card-value-option'
+              key={option.value}
+              onClick={() => this.props.onClick(option.value)}>
+              {option.label}
+            </div>)}
+          </div>
+      </div>
+    )
+  }
+}
+
 class ProjectCard extends Component {
   render() {
     return (
@@ -13,7 +44,7 @@ class ProjectCard extends Component {
             </div>
             <div className='col-md-4'>
               <div className='card-title'>Status</div>
-              <div className='card-value'>{this.props.status}</div>
+              <CardValueDropdown value={this.props.status} options={ProjectStatusOptions}/>
             </div>
           </div>
         </div>
