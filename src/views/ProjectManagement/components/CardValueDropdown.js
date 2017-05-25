09c120099a057f728717f65028857e81ce372379
Christopher Best <bestchris@gmail.com>
2017-05-25 16:29:33 -0400

    fix project and status dropdowns

--- src/views/ProjectManagement/components/CardValueDropdown.js
+++ src/views/ProjectManagement/components/CardValueDropdown.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import enhanceWithClickOutside from 'react-click-outside'
 
 class CardValueDropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focus: false
+    };
+  }
+  toggleFocus() {
+    this.setState(Object.assign({}, this.state, {focus: !this.state.focus}));
+  }
+  handleClickOutside() {
+    this.setState(Object.assign({}, this.state, {focus: false}));
+  }
   render() {
     return (
-      <div className='card-value card-value-select' tabIndex="0">
+      <div className='card-value card-value-select' onClick={this.toggleFocus.bind(this)}>
         {this.props.value}
         <span className='card-value-select-arrow'></span>
-        <div className='card-value-options'>
+        <div
+          className={`card-value-options ${this.state.focus ? 'card-value-options-open' : ''}`}>
           {this.props.options.map(option =>
             <div
               className='card-value-option'
@@ -20,4 +34,4 @@ class CardValueDropdown extends Component {
   }
 }
 
-export default CardValueDropdown;
+export default enhanceWithClickOutside(CardValueDropdown);
