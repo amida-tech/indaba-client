7728fbb38a5a1ac29afeeae2f6969b489cb94327
Christopher Best <bestchris@gmail.com>
2017-05-25 16:47:18 -0400

    improve status card dropdowns

--- src/views/ProjectManagement/components/CardValueDropdown.js
+++ src/views/ProjectManagement/components/CardValueDropdown.js
@@ -18,18 +18,20 @@ class CardValueDropdown extends Component {
     return (
       <div className='card-value card-value-select' onClick={this.toggleFocus.bind(this)}>
         {this.props.value}
-        <span className='card-value-select-arrow'></span>
-        <div
-          className={`card-value-options ${this.state.focus ? 'card-value-options-open' : ''}`}>
-          {this.props.options.map(option =>
-            <div
-              className='card-value-option'
-              key={option.value}
-              onClick={() => this.props.onClick(option.value)}>
-              {option.label}
-            </div>)}
+        <div className='card-value-select-arrow'>
+          <div
+            className={`card-value-options ${this.state.focus ? 'card-value-options-open' : ''}`}>
+            {this.props.options.map(option =>
+              <div
+                className='card-value-option'
+                key={option.value}
+                onClick={() => this.props.onClick(option.value)}>
+                {option.label}
+              </div>)}
           </div>
+        </div>
       </div>
+
     )
   }
 }
