05d9fbee050e14de852e1face40b5a68b8f980b9
jmfadeley <jmfadeley@gmail.com>
2017-05-30 17:39:42 -0400

    Forgot changes to roles.

--- src/views/ProjectManagement/components/Modals/AddStage.js
+++ src/views/ProjectManagement/components/Modals/AddStage.js
@@ -45,7 +45,7 @@ class AddStage extends Component {
   render() {
     const vocab = this.props.vocab;
     const roles = this.props.data.project.workflow.roles.map((role, key) =>
-        ({'value': role, 'label': role, 'key': key})
+        ({'value': role.role, 'label': role.role, 'key': key})
       );
     const description = vocab.PROJECT.DESC_ARRAY[this.state.permissions];
 
