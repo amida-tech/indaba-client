5b4cb3e58963833c854e607bf18e6d5fea8d1604
Christopher Best <bestchris@gmail.com>
2017-05-30 13:48:29 -0400

    move customizations in to css file

--- src/views/ProjectManagement/components/Workflow/InviteUser.js
+++ src/views/ProjectManagement/components/Workflow/InviteUser.js
@@ -16,33 +16,32 @@ class InviteUser extends Component {
   }
   render() {
     return (
-      <Box
-        className='invite-user'
-        direction='column'>
-        <div className='invite-user-header'>Not Listed? Invite by email</div>
-        <Box direction='row'>
-          <TextInput
-            style={{width: '50%'}}
-            placeHolder={this.props.vocab.COMMON.FIRST_NAME}
-            onDOMChange={(evt) => this.setField('firstName', evt)}/>
-          <TextInput
-            style={{width: '50%', marginLeft: '5px'}}
-            placeHolder={this.props.vocab.COMMON.LAST_NAME}
-            onDOMChange={(evt) => this.setField('lastName', evt)}/>
-        </Box>
-        <TextInput
-          style={{marginTop: '5px'}}
-          placeHolder={this.props.vocab.COMMON.EMAIL}
-          onDOMChange={(evt) => this.setField('email', evt)}/>
+      <div className='invite-user'>
         <Box
-          direction='row'
-          justify='end'
-          pad={{between: 'small'}}
-          margin={{top: 'small'}}>
-          <Button secondary={true} label='Clear'/>
-          <Button primary={true} label='Invite'/>
+          direction='column'
+          pad={{between: 'small'}}>
+          <div className='invite-user-header'>Not Listed? Invite by email</div>
+          <Box className='invite-user-name-inputs' direction='row' pad={{between: 'small'}}>
+            <TextInput
+              placeHolder={this.props.vocab.COMMON.FIRST_NAME}
+              onDOMChange={(evt) => this.setField('firstName', evt)}/>
+            <TextInput
+              placeHolder={this.props.vocab.COMMON.LAST_NAME}
+              onDOMChange={(evt) => this.setField('lastName', evt)}/>
+          </Box>
+          <TextInput
+            placeHolder={this.props.vocab.COMMON.EMAIL}
+            onDOMChange={(evt) => this.setField('email', evt)}/>
+          <Box
+            direction='row'
+            justify='end'
+            pad={{between: 'small'}}
+            margin={{top: 'small'}}>
+            <Button secondary={true} label='Clear'/>
+            <Button primary={true} label='Invite'/>
+          </Box>
         </Box>
-      </Box>
+      </div>
     )
   }
 }
