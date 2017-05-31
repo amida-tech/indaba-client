3d63b7bc9deac0417f6c5dda66af7e6a646184fe
Christopher Best <bestchris@gmail.com>
2017-05-31 16:01:47 -0400

    workflow sidebar style

--- src/views/ProjectManagement/components/Workflow/Sidebar.js
+++ src/views/ProjectManagement/components/Workflow/Sidebar.js
@@ -7,7 +7,7 @@ import InviteUser from './InviteUser';
 
 import List from 'grommet/components/List';
 import ListItem from 'grommet/components/ListItem';
-import SearchInput from 'grommet/components/SearchInput';
+import Search from 'grommet/components/Search';
 import Box from 'grommet/components/Box';
 import Select from 'grommet/components/Select';
 
@@ -19,19 +19,26 @@ class Sidebar extends Component {
     return (
       <Box appCentered={false}
         separator='all'
-        pad={{between: 'small', vertical: 'small', horizontal: 'small'}}>
-        <div>{this.props.vocab.PROJECT.DND_INSTRUCTIONS}</div>
-        <SearchInput placeHolder={this.props.vocab.COMMON.SEARCH}/>
-        <Select placeHolder={this.props.vocab.PROJECT.FILTER_BY_GROUP}
-          options={this.props.data.project.workflow.roles} />
-        <List>
-          {unassigned.map(unassignee =>
-            <ListItem key={"Unassigned-"+unassignee.props.children.id}>
-              {unassignee}
-            </ListItem>
-          )}
-        </List>
-        <InviteUser vocab={this.props.vocab} />
+        className='sidebar'>
+        <div className='sidebar-instruction'>{this.props.vocab.PROJECT.DND_INSTRUCTIONS}</div>
+        <div className='sidebar-user-picker'>
+          <Search className='sidebar-user-search'
+            fill={true}
+            placeHolder={this.props.vocab.COMMON.SEARCH}
+            inline={true}/>
+          <Select placeHolder={this.props.vocab.PROJECT.FILTER_BY_GROUP}
+            options={this.props.data.project.workflow.roles} />
+          <List>
+            {unassigned.map(unassignee =>
+              <ListItem key={"Unassigned-"+unassignee.props.children.id}>
+                {unassignee}
+              </ListItem>
+            )}
+          </List>
+        </div>
+        <div>
+          <InviteUser className='sidebard-invite-user' vocab={this.props.vocab} />
+        </div>
       </Box>
     )
   }
