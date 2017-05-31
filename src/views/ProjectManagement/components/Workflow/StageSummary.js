00ff0892d53420ba46262265c8f69b62fb3ef326
Christopher Best <bestchris@gmail.com>
2017-05-31 14:22:09 -0400

    style stage summary cells

--- src/views/ProjectManagement/components/Workflow/StageSummary.js
+++ src/views/ProjectManagement/components/Workflow/StageSummary.js
@@ -10,12 +10,12 @@ class StageSummary extends Component {
           responsive={false}
           className='stage-summary-row'>
           <div className='stage-left'>
-            <div className='stage-date-value'>{this.props.stage.startStage}</div>
-            <div className='stage-date-label'>{this.props.vocab.STAGE.START_STAGE}</div>
+            <div className='stage-summary-value'>{this.props.stage.startStage}</div>
+            <div className='stage-summary-label'>{this.props.vocab.STAGE.START_STAGE}</div>
           </div>
           <div className='stage-right'>
-            <div className='stage-group-value'>{this.props.stage.userGroup}</div>
-            <div className='stage-group-label'>{this.props.vocab.STAGE.USER_GROUP}</div>
+            <div className='stage-summary-value'>{this.props.stage.userGroup}</div>
+            <div className='stage-summary-label'>{this.props.vocab.STAGE.USER_GROUP}</div>
           </div>
         </Box>
         <Box direction='row'
@@ -23,12 +23,12 @@ class StageSummary extends Component {
           responsive={false}
           className='stage-summary-row'>
           <div className='stage-left'>
-            <div className='stage-date'>{this.props.stage.endStage}</div>
-            <div className='stage-date-label'>{this.props.vocab.STAGE.END_STAGE}</div>
+            <div className='stage-summary-value'>{this.props.stage.endStage}</div>
+            <div className='stage-summary-label'>{this.props.vocab.STAGE.END_STAGE}</div>
           </div>
           <div className='stage-right'>
-            <div className='stage-permissions-value'>{this.props.stage.permissions}</div>
-            <div className='stage-permissions-label'>{this.props.vocab.STAGE.PERMISSIONS}</div>
+            <div className='stage-summary-value'>{this.props.stage.permissions}</div>
+            <div className='stage-summary-label'>{this.props.vocab.STAGE.PERMISSIONS}</div>
           </div>
         </Box>
       </div>
