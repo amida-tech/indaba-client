491a61eb1f4ff3c688561984f27ca3138952104f
Christopher Best <bestchris@gmail.com>
2017-06-05 16:31:47 -0400

    fill out nav bar contents

--- src/views/Nav/index.js
+++ src/views/Nav/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { Box } from 'grommet';
+import { Box, Button } from 'grommet';
 
 
 class NavContainer extends Component {
@@ -9,12 +9,19 @@ class NavContainer extends Component {
         return (
             <nav className='nav'>
                 <Box
-                  justify='between'>
-                    <div>
-                        <div className='indaba-logo'>&copy;</div>
-                    </div>
-                    <div>
-                    </div>
+                  justify='between'
+                  direction='row'>
+                    <Box direction='row'>
+                        <div className='nav-indaba-logo'>|indaba-icon|</div>
+                        <Link to='/project'>Project</Link>
+                        <Link to='/users'></Link>
+                        <Link to='/subjects'></Link>
+                        <Button>Create</Button>
+                    </Box>
+                    <Box direction='row'>
+                        <div className='nav-mail'>|envelope-icon|</div>
+                        <div className='nav-account'>|face-icon|</div>
+                    </Box>
                 </Box>
             </nav>
         );
