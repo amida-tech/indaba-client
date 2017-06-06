4d4c63581941d4fea77f79412b5d2ee5486a72cf
Christopher Best <bestchris@gmail.com>
2017-06-06 13:59:22 -0400

    initialize wizard store, reducer, and actions add title to store on save

--- src/views/CreateProjectWizard/components/index.js
+++ src/views/CreateProjectWizard/components/index.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { Tabs, Tab } from 'grommet';
 import _ from 'lodash';
 import ModalContent from '../../ProjectManagement/components/Modals';
+import { setProjectTitle } from '../actions';
 
 class CreateProjectWizard extends Component {
     render() {
         return (
             <div>
                 {!_.get(this.props.wizard, 'title') &&
-                    <ModalContent id='newprojecttitle' />}
+                    <ModalContent id='newprojecttitle' onSave={this.props.onSetTitle}/>}
                 <Tabs>
                     <Tab title='Create Survey'>Create a survey!</Tab>
                     <Tab title='Add Subjects'>Add subjects!</Tab>
@@ -24,5 +25,8 @@ class CreateProjectWizard extends Component {
 const mapStateToProps = state => ({
     wizard: state.projectwizard,
 });
+const mapDispatchToProps = dispatch => ({
+    onSetTitle: title => dispatch(setProjectTitle(title)),
+});
 
-export default connect(mapStateToProps)(CreateProjectWizard);
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProjectWizard);
--- src/views/ProjectManagement/components/Modals/NewProjectTitle.js
+++ src/views/ProjectManagement/components/Modals/NewProjectTitle.js
@@ -3,12 +3,22 @@ import { TextInput } from 'grommet';
 import Modal from '../../../../common/Modal';
 
 class NewProjectTitle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+        };
+    }
+    handleTitleChange(evt) {
+        this.setState({ title: evt.target.value });
+    }
     render() {
         return <Modal
             title='Project Title'
-            class='new-project-title-layer'>
+            class='new-project-title-layer'
+            onSave={() => this.props.onSave(this.state.title)}>
             <div>
-                <TextInput placeholder='Title'/>
+                <TextInput placeholder='Title' onDOMChange={this.handleTitleChange.bind(this)}/>
                 <div className='new-project-title-layer-description-container'>
                     <textarea className='new-project-title-layer-description'/>
                 </div>
