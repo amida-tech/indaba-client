1e3f4148360a10f9872efcd52dbe948bef905b61
Christopher Best <bestchris@gmail.com>
2017-06-06 14:24:13 -0400

    add survey editor stage footer

--- src/views/CreateProjectWizard/components/SurveyEditorStep.js
+++ src/views/CreateProjectWizard/components/SurveyEditorStep.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Footer, Button, Box } from 'grommet';
 
 class SurveyEditorStep extends Component {
     render() {
         return (<div>
-            // <SurveyEditor />
-            // <Footer />
+            &lt;Survey Editor /&gt;
+            <Footer justify='between'>
+                <div>
+                    <Button label='Go Back' />
+                </div>
+                <Box direction='row' pad={{ between: 'small' }}>
+                    <Button label='Cancel' />
+                    <Button label='Skip This Step' />
+                    <Button primary label='Continue' />
+                </Box>
+            </Footer>
         </div>);
     }
 }
--- src/views/CreateProjectWizard/components/index.js
+++ src/views/CreateProjectWizard/components/index.js
@@ -4,6 +4,7 @@ import { Tabs, Tab } from 'grommet';
 import _ from 'lodash';
 import ModalContent from '../../ProjectManagement/components/Modals';
 import { setProjectTitle } from '../actions';
+import SurveyEditorStep from './SurveyEditorStep';
 
 class CreateProjectWizard extends Component {
     render() {
@@ -12,7 +13,9 @@ class CreateProjectWizard extends Component {
                 {!_.get(this.props.wizard, 'title') &&
                     <ModalContent id='newprojecttitle' onSave={this.props.onSetTitle}/>}
                 <Tabs>
-                    <Tab title='Create Survey'>Create a survey!</Tab>
+                    <Tab title='Create Survey'>
+                        <SurveyEditorStep />
+                    </Tab>
                     <Tab title='Add Subjects'>Add subjects!</Tab>
                     <Tab title='Add Users'>Add users!</Tab>
                     <Tab title='Add Stages'>Add stages!</Tab>
