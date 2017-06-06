804145f692953e5979af49b10b2c70d65ec961fe
Christopher Best <bestchris@gmail.com>
2017-06-05 21:07:13 -0400

    declare modal for creating new project

---
+++ src/views/ProjectManagement/components/Modals/CreateNewProject.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+class CreateNewProject extends Component {
+}
+
+export default CreateNewProject;
--- src/views/ProjectManagement/components/Modals/index.js
+++ src/views/ProjectManagement/components/Modals/index.js
@@ -2,42 +2,47 @@ import React, { Component } from 'react';
 import AddSubject from './AddSubject';
 import AddStage from './AddStage';
 import StatusChange from './StatusChange';
+import CreateNewProject from './CreateNewProject';
 
 const modalIDToComponent = {
-  addsubject: {
-    component: AddSubject
-  },
-  addstage: {
-    component: AddStage
-  },
-  projectstatuschange: {
-    component: StatusChange,
-    props: {
-      entity: 'project'
-    }
-  },
-  surveystatuschange: {
-    component: StatusChange,
-    props: {
-      entity: 'survey'
-    }
-  }
+    addsubject: {
+        component: AddSubject,
+    },
+    addstage: {
+        component: AddStage,
+    },
+    projectstatuschange: {
+        component: StatusChange,
+        props: {
+            entity: 'project',
+        },
+    },
+    surveystatuschange: {
+        component: StatusChange,
+        props: {
+            entity: 'survey',
+        },
+    },
+    createnewproject: {
+        component: CreateNewProject,
+    },
 };
 
 class ModalContent extends Component {
-  render() {
-    const ContentComponent = modalIDToComponent[this.props.id].component;
-    return (
+    render() {
+        const ContentComponent = modalIDToComponent[this.props.id].component;
+        return (
         <ContentComponent {...this.props} {...modalIDToComponent[this.props.id].props}/>
-    )
-  }
+        );
+    }
 }
 
 export const modalIDs = {
-  ADD_SUBJECT_MODAL: 'addsubject',
-  ADD_STAGE_MODAL: 'addstage',
-  PROJECT_STATUS_MODAL: 'projectstatuschange',
-  SURVEY_STATUS_MODAL: 'surveystatuschange'
-}
+    ADD_SUBJECT_MODAL: 'addsubject',
+    ADD_STAGE_MODAL: 'addstage',
+    PROJECT_STATUS_MODAL: 'projectstatuschange',
+    SURVEY_STATUS_MODAL: 'surveystatuschange',
+    CREATE_NEW_PROJECT: 'CreateNewProject',
+};
 
 export default ModalContent;
