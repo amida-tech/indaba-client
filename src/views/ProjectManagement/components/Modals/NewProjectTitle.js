7682e5abc3257b008b03785f5ceff111540a8f3b
Christopher Best <bestchris@gmail.com>
2017-06-05 23:05:50 -0400

    add new project title modal content

--- src/views/ProjectManagement/components/Modals/NewProjectTitle.js
+++ src/views/ProjectManagement/components/Modals/NewProjectTitle.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import Modal from '../../../../common/Modal';
+import { TextInput } from 'grommet';
 
 class NewProjectTitle extends Component {
+    render() {
+        return <Modal
+            title='Project Title'
+            class='new-project-title-layer'
+            content={
+                <div>
+                    <TextInput placeholder='Title'/>
+                    <div className='new-project-title-layer-description-container'>
+                        <textarea className='new-project-title-layer-description'/>
+                    </div>
+                </div>
+            }
+            data={this.props.data}/>;
+    }
 }
 
 export default NewProjectTitle;
