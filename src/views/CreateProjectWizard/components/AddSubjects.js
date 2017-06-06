aca91556ba3020c814af35f87d439617bb5af6a5
Christopher Best <bestchris@gmail.com>
2017-06-06 15:31:07 -0400

    start of actual subject add list control

--- src/views/CreateProjectWizard/components/AddSubjects.js
+++ src/views/CreateProjectWizard/components/AddSubjects.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
-import { Box, Button } from 'grommet';
+import { Box, Button, TextInput } from 'grommet';
+import IonIcon from 'react-ionicons';
+
+class SubjectListEntry extends Component {
+
+}
+
+class AddSubjectControl extends Component {
+    render() {
+        return <div>
+            <Box direction='row' justify='between'>
+                <div>Subjects are units of analysis.
+                    For example, cities, programs, or companies.
+                    Separate subjects with a comma.</div>
+                <div>Actions</div>
+            </Box>
+            <Box><TextInput /><IonIcon icon='ion-ios-plus'/></Box>
+        </div>;
+    }
+}
 
 class AddSubjects extends Component {
     render() {
@@ -10,6 +29,10 @@ class AddSubjects extends Component {
                 pad={{ vertical: 'small', horizontal: 'medium' }}>
                 <Button label='Import Subjects' />
             </Box>
+            <hr className='divider'/>
+            <p>Add survey subjects or use the import manager to add subjects
+                from a previous project.</p>
+            <AddSubjectControl />
         </div>;
     }
 }
