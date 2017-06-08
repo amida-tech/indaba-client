b8544a355553b5ad8e8763aa56b1e98e2e129037
Christopher Best <bestchris@gmail.com>
2017-06-07 22:57:25 -0400

    sync add user group inputs with state

--- src/views/CreateProjectWizard/components/AddUserGroup.js
+++ src/views/CreateProjectWizard/components/AddUserGroup.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 import { TextInput, Box } from 'grommet';
+import update from 'immutability-helper';
 import Modal from '../../../common/Modal';
 
 class AddUserGroup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            groupName: '',
+            usersQuery: '',
+            groupQuery: '',
+        };
+
+        this.handleGroupName = this.handleGroupName.bind(this);
+        this.handleUsersQuery = this.handleUsersQuery.bind(this);
+        this.handleGroupQuery = this.handleGroupQuery.bind(this);
+    }
+    handleGroupName(evt) {
+        this.setState(update(this.state, { groupName: { $set: evt.target.value } }));
+    }
+    handleUsersQuery(evt) {
+        this.setState(update(this.state, { usersQuery: { $set: evt.target.value } }));
+    }
+    handleGroupQuery(evt) {
+        this.setState(update(this.state, { groupQuery: { $set: evt.target.value } }));
+    }
     render() {
         return (
             <Modal
                 title={this.props.vocab.PROJECT.ADD_USER_GROUP}>
                 <Box pad={{ between: 'small', horizontal: 'small', vertical: 'small' }}>
                     {this.props.vocab.STAGE.GROUP_NAME}
-                    <TextInput placeHolder={this.props.vocab.PROJECT.ENTER_GROUP_NAME} />
+                    <TextInput
+                        placeHolder={this.props.vocab.PROJECT.ENTER_GROUP_NAME}
+                        onDOMChange={this.handleGroupName}/>
                     <Box direction='row'>
                         <Box separator='all'
                             pad='small'>
                             {this.props.vocab.COMMON.ALL_USERS}
-                            <TextInput placeHolder={this.props.vocab.COMMON.SEARCH}/>
+                            <TextInput
+                                placeHolder={this.props.vocab.COMMON.SEARCH}
+                                onDOMChange={this.handleUsersQuery}/>
                         </Box>
                         <Box justify='center'
                             pad='small'>
@@ -26,7 +52,9 @@ class AddUserGroup extends Component {
                         <Box separator='all'
                             pad='small'>
                             {this.props.vocab.STAGE.USER_GROUP}
-                            <TextInput placeHolder={this.props.vocab.COMMON.SEARCH}/>
+                            <TextInput
+                                placeHolder={this.props.vocab.COMMON.SEARCH}
+                                onDOMChange={this.handleGroupQuery}/>
                         </Box>
                     </Box>
                 </Box>
