faff495f35cb8d4a3a4d8b1b20d7fa3b17199e6d
Christopher Best <bestchris@gmail.com>
2017-06-09 16:10:08 -0400

    add summary to add stages step

--- src/views/CreateProjectWizard/components/AddStages.js
+++ src/views/CreateProjectWizard/components/AddStages.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Summary from '../../../common/components/Summary';
 
 class AddStages extends Component {
     render() {
-        return (<div>
+        return (<div className='add-stages-step'>
+            <Summary
+                project={this.props.project}
+                survey={this.props.survey}
+                vocab={this.props.vocab} />
+            <hr className='divider'/>
         </div>);
     }
 }
 
-export default AddStages;
+const mapStateToProps = state => ({
+    vocab: state.settings.language.vocabulary,
+    project: state.projectwizard.project,
+    survey: state.projectwizard.survey,
+});
+
+export default connect(mapStateToProps)(AddStages);
