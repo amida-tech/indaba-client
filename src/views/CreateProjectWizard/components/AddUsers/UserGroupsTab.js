aa42512016d10555b0a58d088b887577019628a2
Christopher Best <bestchris@gmail.com>
2017-06-09 13:08:15 -0400

    add name and delete icon

--- src/views/CreateProjectWizard/components/AddUsers/UserGroupsTab.js
+++ src/views/CreateProjectWizard/components/AddUsers/UserGroupsTab.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Box } from 'grommet';
+import IonIcon from 'react-ionicons';
 
 class UserGroupsTab extends Component {
     render() {
@@ -7,13 +8,16 @@ class UserGroupsTab extends Component {
             <Box className='user-groups-tab'
                 pad={{ horizontal: 'medium', vertical: 'medium', between: 'small' }}>
                 {this.props.userGroups.map(group =>
-                <Box direction='row'
+                <Box key={group.id}
+                    direction='row'
                     justify='between'>
                     <div className='user-group-list__name'>
+                        {group.name}
                     </div>
                     <div className='user-group-list__badge-string'>
                     </div>
                     <div className='user-group-list__delete'>
+                        <IonIcon icon='ion-android-delete' />
                     </div>
                 </Box>)}
             </Box>
