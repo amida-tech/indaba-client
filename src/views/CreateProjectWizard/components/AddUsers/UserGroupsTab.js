70676953e50f599cf305d5b6bd1617e7d34ebf90
Christopher Best <bestchris@gmail.com>
2017-06-09 14:30:05 -0400

    implement user group search box

--- src/views/CreateProjectWizard/components/AddUsers/UserGroupsTab.js
+++ src/views/CreateProjectWizard/components/AddUsers/UserGroupsTab.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import { Box } from 'grommet';
+import { Box, TextInput } from 'grommet';
 import IonIcon from 'react-ionicons';
 import UserBadge from './UserBadge';
 
 class UserGroupsTab extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: '',
+        };
+    }
+    filterGroup(group) {
+        return group.name.toLowerCase().includes(this.state.query.toLowerCase());
+    }
     lookupUser(userId) {
         return this.props.allUsers.find(user => user.id === userId);
     }
@@ -11,7 +20,12 @@ class UserGroupsTab extends Component {
         return (
             <Box className='user-groups-tab'
                 pad={{ horizontal: 'medium', vertical: 'medium', between: 'small' }}>
+                <div className='user-groups-tab__search-header'>
+                    <TextInput placeHolder={this.props.vocab.PROJECT.SEARCH_FOR_USER_GROUPS}
+                        onDOMChange={evt => this.setState({ query: evt.target.value })}/>
+                </div>
                 {this.props.userGroups.map(group =>
+                    this.filterGroup(group) &&
                 <Box key={group.id}
                     direction='row'
                     justify='between'>
