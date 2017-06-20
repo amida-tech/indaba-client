6d02ce30856ab4084e24ef9099e4a0e77c58b79d
Christopher Best <bestchris@gmail.com>
2017-06-20 13:49:43 -0400

    add account tab stub

---
+++ src/views/ProjectManagement/components/Users/UserProfile/AccountTab.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AccountTab extends Component {
+    render() {
+        return (
+            <div className='account-tab'>
+            </div>
+        );
+    }
+}
+
+export default AccountTab;
