088370728cd8df8c4a625a9ed45af200240653c3
Christopher Best <bestchris@gmail.com>
2017-06-20 15:28:53 -0400

    remove permission from users

--- src/common/reducers/userReducer.js
+++ src/common/reducers/userReducer.js
@@ -7,47 +7,38 @@ const initialState = {
         id: 0,
         firstName: 'Tyler',
         lastName: 'Durden',
-        permissions: 4,
     }, {
         id: 1,
         firstName: 'Robert',
         lastName: 'Paulson',
-        permissions: 0,
     }, {
         id: 2,
         firstName: 'Jon',
         lastName: 'McLane',
-        permissions: 0,
     }, {
         id: 3,
         firstName: 'Ellen',
         lastName: 'Ripley',
-        permissions: 0,
     }, {
         id: 4,
         firstName: 'Indiana',
         lastName: 'Jones',
-        permissions: 0,
     }, {
         id: 5,
         firstName: 'Tony',
         lastName: 'Stark',
-        permissions: 0,
     }, {
         id: 6,
         firstName: 'Johnny',
         lastName: 'Quest',
-        permissions: 0,
     }, {
         id: 7,
         firstName: 'Buck',
         lastName: 'Rogers',
-        permissions: 0,
     }, {
         id: 8,
         firstName: 'Marvin',
         lastName: 'Martian',
-        permissions: 0,
     }],
 };
 
