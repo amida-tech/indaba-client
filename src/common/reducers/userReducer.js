94273d5756c7954dfa7e2c1771eed1bc56e6b559
Christopher Best <bestchris@gmail.com>
2017-06-20 15:36:01 -0400

    add additional user attributes to store

--- src/common/reducers/userReducer.js
+++ src/common/reducers/userReducer.js
@@ -7,38 +7,49 @@ const initialState = {
         id: 0,
         firstName: 'Tyler',
         lastName: 'Durden',
+        email: 't.durden@visa.com',
+        activationDate: '1/1/2016',
     }, {
         id: 1,
         firstName: 'Robert',
         lastName: 'Paulson',
+        email: 'bobbyp@dove.com',
     }, {
         id: 2,
-        firstName: 'Jon',
-        lastName: 'McLane',
+        firstName: 'John',
+        lastName: 'McClane',
+        email: 'johnm@nypd.gov',
+        title: 'Detective Lieutenant',
     }, {
         id: 3,
         firstName: 'Ellen',
         lastName: 'Ripley',
+        email: 'believeit@snopes.com',
     }, {
         id: 4,
         firstName: 'Indiana',
         lastName: 'Jones',
+        email: 'digem@cmu.edu',
     }, {
         id: 5,
         firstName: 'Tony',
         lastName: 'Stark',
+        email: 'FeMan@stark.com',
     }, {
         id: 6,
         firstName: 'Johnny',
         lastName: 'Quest',
+        email: 'john@gmail.com',
     }, {
         id: 7,
         firstName: 'Buck',
         lastName: 'Rogers',
+        email: 'thebuckster@americanradgas.com',
     }, {
         id: 8,
         firstName: 'Marvin',
         lastName: 'Martian',
+        email: 'bleepblorp@mars.com',
     }],
 };
 
