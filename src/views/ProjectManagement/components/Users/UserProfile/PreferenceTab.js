a0535be2bb37dc93717d8676003d2422c2460034
Christopher Best <bestchris@gmail.com>
2017-06-20 13:57:28 -0400

    add preference tab stub

---
+++ src/views/ProjectManagement/components/Users/UserProfile/PreferenceTab.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PreferenceTab extends Component {
+    render() {
+        return (
+            <div className='preference-tab'></div>
+        );
+    }
+}
+
+export default PreferenceTab;
