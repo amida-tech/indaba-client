28e6aa9a57c90e3c73ae2076c6651e6f4b19a7d7
Christopher Best <bestchris@gmail.com>
2017-06-20 13:55:02 -0400

    add tasks tab stub

---
+++ src/views/ProjectManagement/components/Users/UserProfile/TasksTab.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TasksTab extends Component {
+    render() {
+        return (
+            <div className='tasks-tab'></div>
+        );
+    }
+}
+
+export default TasksTab;
