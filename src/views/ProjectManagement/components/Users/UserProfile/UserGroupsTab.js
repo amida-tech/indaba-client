29bbcfbd0e66b91e3a81c6c13c84772d83c1955a
Christopher Best <bestchris@gmail.com>
2017-06-20 13:54:31 -0400

    add user groups tab stub

---
+++ src/views/ProjectManagement/components/Users/UserProfile/UserGroupsTab.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class UserGroupsTab extends Component {
+    render() {
+        return (
+            <div className='user-groups-tab'>
+            </div>
+        );
+    }
+}
+
+export default UserGroupsTab;
