9b8a518eba73e9ad7de3640d231cd344d1babb0b
Christopher Best <bestchris@gmail.com>
2017-06-20 16:26:15 -0400

    start shared group list component

---
+++ src/common/components/UserGroupList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UserGroupList extends Component {
+    render() {
+        return (
+            <div className='user-group-list'></div>
+        );
+    }
+}
+
+export default UserGroupList;
