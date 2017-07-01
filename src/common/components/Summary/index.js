45c4a96446866f0420cf986e20059c7f3e1dd79f
Christopher Best <bestchris@gmail.com>
2017-06-30 23:08:22 -0400

    unify status cards remove extra dropdown click to open status change modal

---
+++ src/common/components/Summary/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import IonIcon from 'react-ionicons';
+
+import StatusCard from './StatusCard';
+
+class Summary extends Component {
+    render() {
+        return (
+            <div className='summary'>
+                <StatusCard
+                    label={this.props.vocab.PROJECT.PROJECT}
+                    name={this.props.project.name}
+                    status={this.props.project.status}
+                    onStatusChangeClick={() => this.props.onStatusChangeClick('projectstatusmodal')}/>
+                <StatusCard
+                    label={this.props.vocab.PROJECT.SURVEY}
+                    name={this.props.survey.name}
+                    status={this.props.survey.status}
+                    onStatusChangeClick={() => this.props.onStatusChangeClick('surveystatusmodal')}>
+                    <IonIcon
+                        icon='ion-ios-paper-outline'
+                        color='#4EB276'
+                        fontSize='4em'
+                        className='status-card-icon' />
+                </StatusCard>
+            </div>
+        );
+    }
+}
+
+Summary.propTypes = {
+    project: PropTypes.object.isRequired,
+    survey: PropTypes.object.isRequired,
+    vocab: PropTypes.object.isRequired,
+
+    onStatusChangeClick: PropTypes.func,
+};
+
+export default Summary;
