e02f2cedf59f349eca12917646e94ca9d043930d
Christopher Best <bestchris@gmail.com>
2017-07-11 14:57:20 -0400

    fix cancel button remove old methods and state

--- src/views/ProjectManagement/components/Workflow/AssigneeContainer/InviteUser.js
+++ src/views/ProjectManagement/components/Workflow/AssigneeContainer/InviteUser.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, reset } from 'redux-form';
 import { Button } from 'grommet';
 
 class InviteUser extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            id: 41,
-        };
-    }
-    setField(field, evt) {
-        this.setState(Object.assign({}, this.state,
-            { [field]: evt.target.value }));
-    }
-    clear() {
-        this.setState({ firstName: '', lastName: '', email: '' });
-    }
     render() {
         return (
             <form className='invite-user' onSubmit={this.props.handleSubmit}>
@@ -44,7 +29,7 @@ class InviteUser extends Component {
                 <div className='invite-user__buttons'>
                     <Button className='invite-user__button'
                         secondary={true} label={this.props.vocab.COMMON.CLEAR}
-                        onClick={this.clear.bind(this)}/>
+                        onClick={this.props.onClear}/>
                     <Button className='invite-user__button'
                         type='submit'
                         primary={true} label={this.props.vocab.COMMON.INVITE}/>
@@ -56,7 +41,11 @@ class InviteUser extends Component {
 
 InviteUser.propTypes = {
     vocab: PropTypes.object.isRequired,
-    onInviteUser: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
 };
 
-export default reduxForm({ form: 'user-sidebar-invite-user' })(InviteUser);
+const FORM_NAME = 'user-sidebar-invite-user';
+
+export default connect(null, dispatch => ({
+    onClear: () => dispatch(reset(FORM_NAME)),
+}))(reduxForm({ form: FORM_NAME })(InviteUser));
