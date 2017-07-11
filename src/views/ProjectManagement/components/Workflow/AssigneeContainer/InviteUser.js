f809c0cab7508744b996078680df8daecb054ffb
Christopher Best <bestchris@gmail.com>
2017-07-11 13:53:33 -0400

    remove extra Box

--- src/views/ProjectManagement/components/Workflow/AssigneeContainer/InviteUser.js
+++ src/views/ProjectManagement/components/Workflow/AssigneeContainer/InviteUser.js
@@ -23,39 +23,35 @@ class InviteUser extends Component {
     render() {
         return (
             <div className='invite-user'>
-                <Box
-                    direction='column'
-                    pad={{ between: 'small' }}>
-                    <div className='invite-user__header'>
-                        {this.props.vocab.PROJECT.INVITE_INSTRUCTION}
-                    </div>
-                    <Box className='invite-user__name-inputs' direction='row' pad={{ between: 'small' }}>
-                        <TextInput
-                            placeHolder={this.props.vocab.COMMON.FIRST_NAME}
-                            onDOMChange={evt => this.setField('firstName', evt)}
-                            value={this.state.firstName}/>
-                        <TextInput
-                            placeHolder={this.props.vocab.COMMON.LAST_NAME}
-                            onDOMChange={evt => this.setField('lastName', evt)}
-                            value={this.state.lastName}/>
-                    </Box>
+                <div className='invite-user__header'>
+                    {this.props.vocab.PROJECT.INVITE_INSTRUCTION}
+                </div>
+                <Box className='invite-user__name-inputs' direction='row' pad={{ between: 'small' }}>
                     <TextInput
-                        placeHolder={this.props.vocab.COMMON.EMAIL}
-                        className='invite-user__email'
-                        onDOMChange={evt => this.setField('email', evt)}
-                        value={this.state.email}/>
-                    <Box
-                        className='invite-user__buttons'
-                        direction='row'
-                        justify='end'
-                        pad={{ between: 'small' }}
-                        margin={{ top: 'small' }}>
+                        placeHolder={this.props.vocab.COMMON.FIRST_NAME}
+                        onDOMChange={evt => this.setField('firstName', evt)}
+                        value={this.state.firstName}/>
+                    <TextInput
+                        placeHolder={this.props.vocab.COMMON.LAST_NAME}
+                        onDOMChange={evt => this.setField('lastName', evt)}
+                        value={this.state.lastName}/>
+                </Box>
+                <TextInput
+                    placeHolder={this.props.vocab.COMMON.EMAIL}
+                    className='invite-user__email'
+                    onDOMChange={evt => this.setField('email', evt)}
+                    value={this.state.email}/>
+                <Box
+                    className='invite-user__buttons'
+                    direction='row'
+                    justify='end'
+                    pad={{ between: 'small' }}
+                    margin={{ top: 'small' }}>
 
-                        <Button secondary={true} label={this.props.vocab.COMMON.CLEAR}
-                            onClick={this.clear.bind(this)}/>
-                        <Button primary={true} label={this.props.vocab.COMMON.INVITE}
-                            onClick={() => this.props.onInviteUser(this.state)}/>
-                    </Box>
+                    <Button secondary={true} label={this.props.vocab.COMMON.CLEAR}
+                        onClick={this.clear.bind(this)}/>
+                    <Button primary={true} label={this.props.vocab.COMMON.INVITE}
+                        onClick={() => this.props.onInviteUser(this.state)}/>
                 </Box>
             </div>
         );
