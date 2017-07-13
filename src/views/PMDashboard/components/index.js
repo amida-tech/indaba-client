2f26ae2f196c2860bce1169f24e6307abd2926b1
Christopher Best <bestchris@gmail.com>
2017-07-13 16:06:34 -0400

    add search bar to dashboard page

--- src/views/PMDashboard/components/index.js
+++ src/views/PMDashboard/components/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as actions from '../actions';
 
 class PMDashboard extends Component {
     render() {
         return (
-            <div className='pm-dashboard'></div>
+            <div className='pm-dashboard'>
+                <input type='text'
+                    placeholder={this.props.vocab.COMMON.SEARCH}
+                    onChange={evt => this.props.actions.setSearchQuery(evt.target.value)}/>
+            </div>
         );
     }
 }
 
-export default PMDashboard;
+const mapStateToProps = state => ({
+    vocab: state.settings.language.vocabulary,
+});
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(Object.assign({}, actions), dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PMDashboard);
