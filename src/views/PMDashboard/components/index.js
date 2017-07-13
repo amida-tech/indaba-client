1de64dd4cdc4cd37591b43d7a20587a8f1956233
Christopher Best <bestchris@gmail.com>
2017-07-13 15:56:38 -0400

    add PMDashboard component stub

---
+++ src/views/PMDashboard/components/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PMDashboard extends Component {
+    render() {
+        return (
+            <div className='pm-dashboard'></div>
+        );
+    }
+}
+
+export default PMDashboard;
