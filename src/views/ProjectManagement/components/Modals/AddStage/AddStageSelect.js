64696e4598d39f7791ead26024a9f1f0ee58f56d
Lacey Irvin <irvinlacey1988@gmail.com>
2017-07-23 18:58:30 -0400

    add stage changed user group label color from blue to green

--- src/views/ProjectManagement/components/Modals/AddStage/AddStageSelect.js
+++ src/views/ProjectManagement/components/Modals/AddStage/AddStageSelect.js
@@ -3,8 +3,10 @@ import Select from 'react-select';
 
 class AddStageSelect extends Component {
     render() {
+        console.log('called my class');
         return (
-            <Select // Using a spread operator doesn't play well.
+            <Select
+            className='add-stage-form__select' // Using a spread operator doesn't play well.
                 value={this.props.input.value}
                 onChange={this.props.input.onChange}
                 options={this.props.groups}
