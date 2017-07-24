a03c8897c113a591264fd00e8be096efc0b9a76f
Christopher Best <bestchris@gmail.com>
2017-07-24 15:46:36 -0400

    initialize UserDashboard component

---
+++ src/views/UserDashboard/components/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UserDashboard extends Component {
+    render() {
+        return (
+            <div className='user-dashboard'></div>
+        );
+    }
+}
+
+export default UserDashboard;
