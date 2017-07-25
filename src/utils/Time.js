541fde3e4761bb2cef25640cfe34b5652063b060
Christopher Best <bestchris@gmail.com>
2017-07-25 14:49:40 -0400

    add vocab for due date rendering and Time function

--- src/utils/Time.js
+++ src/utils/Time.js
@@ -7,4 +7,22 @@ export default {
     renderForMessageList(time) {
         return moment(time).format('h:mmA');
     },
+    renderDueDateForTaskList(time, vocab) {
+        moment().calendar(time, {
+            sameDay(now) {
+                return (this.isBefore(now) ?
+                    `[${vocab.TIME.OVERDUE}]` :
+                    `[${vocab.TIME.DUE_TODAY}]`);
+            },
+            nextDay: vocab.TIME.DUE_TOMORROW,
+            nextWeek: 'M.D.YYYY',
+            lastDay: `[${vocab.TIME.OVERDUE}]`,
+            lastWeek: `[${vocab.TIME.OVERDUE}]`,
+            sameElse(now) {
+                return (this.isBefore(now) ?
+                `[${vocab.TIME.OVERDUE}]` :
+                'M.D.YYYY');
+            },
+        });
+    },
 };
