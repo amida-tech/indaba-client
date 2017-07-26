e5dc508bf03bafd492674aeaa2b9fc4741d7631c
Christopher Best <bestchris@gmail.com>
2017-07-26 08:59:47 -0400

    extract status label component

---
+++ src/common/components/StatusLabel.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const StatusLabelType = {
+    BAD: 'BAD',
+    GOOD: 'GOOD',
+    NEUTRAL: 'NEUTRAL',
+};
+
+const _modifiers = {
+    [StatusLabelType.BAD]: 'bad',
+    [StatusLabelType.GOOD]: 'good',
+    [StatusLabelType.NEUTRAL]: 'neutral',
+};
+
+const StatusLabel = ({ label, type }) =>
+    <div className={`status-label status-label--${_modifiers[type]}`}>{label}</div>;
+
+StatusLabel.propTypes = {
+    type: PropTypes.oneOf(Object.keys(StatusLabelType)).isRequired,
+    label: PropTypes.string.isRequired,
+};
+
+export default StatusLabel;
