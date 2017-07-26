958f9ccd0149d2ac91d4f7cfb226b315f296d292
Christopher Best <bestchris@gmail.com>
2017-07-26 14:26:22 -0400

    fix search bar width restriction

--- src/views/UserDashboard/components/UserTaskListControls.js
+++ src/views/UserDashboard/components/UserTaskListControls.js
@@ -8,9 +8,10 @@ class UserTaskListControls extends Component {
     render() {
         return (
             <div className='user-task-list-controls'>
-                <SearchInput className='user-task-list-controls__search'
-                    placeholder={this.props.vocab.COMMON.SEARCH}
-                    onChange={evt => this.props.actions.setSearchQuery(evt.target.value)}/>
+                <div className='user-task-list-controls__search'>
+                    <SearchInput placeholder={this.props.vocab.COMMON.SEARCH}
+                        onChange={evt => this.props.actions.setSearchQuery(evt.target.value)}/>
+                </div>
                 <UserTaskFilter active={this.props.filter}
                     vocab={this.props.vocab}
                     onSetFilter={this.props.actions.setFilter} />
