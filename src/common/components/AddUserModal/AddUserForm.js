4ef3eec9eb2a337e6005f07ad40697a7e96ec0b1
Christopher Best <bestchris@gmail.com>
2017-07-26 11:24:16 -0400

    fixed BEM naming and styles

--- src/common/components/AddUserModal/AddUserForm.js
+++ src/common/components/AddUserModal/AddUserForm.js
@@ -6,26 +6,25 @@ import { Field, reduxForm } from 'redux-form';
 class AddUserForm extends Component {
     render() {
         return (
-            <div className='add-user-modal'>
-            <form className='add-user-modal add-user-modal--form' onSubmit={this.props.handleSubmit}>
-                <label className='add-user-modal add-user-modal--label'>
+            <form className='add-user-form'
+                onSubmit={this.props.handleSubmit}>
+                <label className='add-user-form__label'>
                     {this.props.vocab.COMMON.FIRST_NAME}
                     <Field name='firstName' component='input' type='text'/>
                 </label>
-                <label className='add-user-modal add-user-modal--label'>
+                <label className='add-user-form__label'>
                     {this.props.vocab.COMMON.LAST_NAME}
                     <Field name='lastName' component='input' type='text'/>
                 </label>
-                <label className='add-user-modal add-user-modal--label'>
+                <label className='add-user-form__label'>
                     {this.props.vocab.COMMON.EMAIL}
                     <Field name='email' component='input' type='text'/>
                 </label>
-                <label className='add-user-modal add-user-modal--label'>
+                <label className='add-user-form__label'>
                     {this.props.vocab.PROJECT.TITLE_OPTIONAL}
                     <Field name='title' component='input' type='text'/>
                 </label>
             </form>
-            </div>
         );
     }
 }
