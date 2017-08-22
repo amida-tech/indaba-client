54350d723ed0bf3acc8bfc69f6e80440aa03be1d
jmfadeley <jmfadeley@gmail.com>
2017-08-22 01:12:03 -0400

    Fixed issue that was keeping the MatrixContainer from loading after creating a project.

--- src/views/ProjectManagement/components/Workflow/MatrixContainer.js
+++ src/views/ProjectManagement/components/Workflow/MatrixContainer.js
@@ -53,7 +53,8 @@ class MatrixContainer extends Component {
                           {this.props.project.subjects.map((subject, key) =>
                             <FilteredRow key={key}
                               subject={{ name: subject, key }}
-                              surveySize={this.props.survey.questions.length}
+                              surveySize={this.props.survey.questions ?
+                                  this.props.survey.questions.length : 0}
                               stages={this.props.project.stages}
                               tasks={this.props.tasks}
                               responses={this.props.responses}
