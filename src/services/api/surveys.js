56b2205d46156be5fa2aa39eaa78f96b4c2c10c1
jmfadeley <jmfadeley@gmail.com>
2017-09-25 14:43:08 -0400

    Just a TODO.

--- src/services/api/surveys.js
+++ src/services/api/surveys.js
@@ -1,6 +1,6 @@
 import * as requests from './requests';
 // import getFullPath from '../../utils/getFullPath';
-// You will need to change getFullPath to instead call the microservice.
+// TODO: You will need to change getFullPath to instead call the microservice.
 
 const surveys = {
     getSurvey: (projectId, callback) => {
