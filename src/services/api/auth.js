f59805684ec35caae43608eeaca8e4f3e638d28c
Perry Ogwuche <perryogwuche1@gmail.com>
2017-10-06 12:16:30 -0400

    added version for auth api route

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -5,7 +5,7 @@ const rootURI = config.AUTH_API_HTTPS_URL;
 
 const auth = {
     login: (authPayload, callback) => { // Auth does not use cookies for login.
-        const path = `${rootURI}/api/auth/login`;
+        const path = `${rootURI}/api/v0/auth/login`;
         const body = `username=${authPayload.username}&password=${authPayload.password}`;
         requests.apiAuthPostRequest(path, body, callback);
     },
