48907164403efdf8006a32c01213b560576451fe
Perry Ogwuche <perryogwuche1@gmail.com>
2017-10-20 13:16:32 -0400

    removed console log statement

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -6,7 +6,6 @@ const rootURI = config.AUTH_API_HTTPS_URL;
 const auth = {
     login: (authPayload, callback) => { // Auth does not use cookies for login.
         const path = `${rootURI}/auth/login`;
-        console.log(`PATH IS: ${path}`);
         const body = `username=${authPayload.username}&password=${authPayload.password}`;
         requests.apiAuthPostRequest(path, body, callback);
     },
