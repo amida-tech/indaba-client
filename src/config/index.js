24200f1320387704340f027908cb82d62878cd6c
jmfadeley <jmfadeley@gmail.com>
2017-10-20 13:22:08 -0400

    Just a tweak for linting needs.

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,9 @@
 import devConfig from './development';
 import stagingConfig from './staging';
 
-const config = devConfig;
+let config = devConfig; // eslint-disable-line prefer-const, import/no-mutable-exports
 
-if (process.env.NODE_ENV === 'development') {
-    config = devConfig;
-} else if (process.env.NODE_ENV === 'staging') {
+if (process.env.NODE_ENV === 'staging') {
     config = stagingConfig;
 }
 
