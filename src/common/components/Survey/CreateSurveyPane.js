89bfb046253019876b33f4ce3e6a6bac3b7dd782
jmfadeley <jmfadeley@gmail.com>
2017-10-23 16:25:12 -0400

    Tabs. Some new components. I can't wait to not be interrupted working on this. I really can't.

--- src/common/components/Survey/CreateSurveyPane.js
+++ src/common/components/Survey/CreateSurveyPane.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'grommet';
+
+import NewQuestions from './NewQuestions';
+import ExistingQuestions from './ExistingQuestions';
+
 
 class CreateSurveyPane extends Component {
 
@@ -8,6 +13,14 @@ class CreateSurveyPane extends Component {
                 <div className='create-survey-pane__instructions'>
                     {this.props.vocab.SURVEY.PANE_INSTRUCTIONS}
                 </div>
+                <Tabs justify='start'>
+                    <Tab title={this.props.vocab.SURVEY.NEW_QUESTIONS}>
+                        <NewQuestions {...this.props}/>
+                    </Tab>
+                    <Tab title={this.props.vocab.SURVEY.EXISTING}>
+                        <ExistingQuestions {...this.props}/>
+                    </Tab>
+                </Tabs>
             </div>
         );
     }
---
+++ src/common/components/Survey/NewQuestions.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewQuestions extends Component {
+
+    render() {
+        return (
+            <div>
+                I question your types.
+            </div>
+        );
+    }
+}
+
+export default NewQuestions;
