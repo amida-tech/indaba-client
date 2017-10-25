c500a7d0d7b991dedbc76d8f444d1f3a560f3988
Christopher Best <bestchris@gmail.com>
2017-10-25 14:11:17 -0400

    fix environment variable with messaging service port

--- src/config/development.js
+++ src/config/development.js
@@ -7,6 +7,6 @@ export default {
     AUTH_API_HTTPS_URL: process.env.AUTH_API_HTTPS_URL || 'https://localhost:4000/api/v0',
     REALM: process.env.REALM || 'testorg',
 
-    MESSAGING_API_HTTP_URL: process.env.AUTH_API_HTTP_URL || 'http://localhost:4002',
-    MESSAGING_API_HTTPS_URL: process.env.AUTH_API_HTTPS_URL || 'http://localhost:4002',
+    MESSAGING_API_HTTP_URL: process.env.MESSAGING_API_HTTP_URL || 'http://localhost:4002',
+    MESSAGING_API_HTTPS_URL: process.env.MESSAGING_API_HTTPS_URL || 'http://localhost:4002',
 };
