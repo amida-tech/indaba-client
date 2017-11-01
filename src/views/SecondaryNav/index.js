5bd64b9b55961937f96d40054d7698e81acaa229
Lacey Irvin <irvinlacey1988@gmail.com>
2017-11-01 15:27:36 -0400

    added seperate scss file for the secondary nav and copied primary nav styles over

--- src/views/SecondaryNav/index.js
+++ src/views/SecondaryNav/index.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-import { Box } from 'grommet';
+
 
 class SecondaryNavContainer extends Component {
     render() {
         return (
-            <nav className='primary-nav'>
-                <Box
-                  justify='between'
-                  direction='row'
-                  align='center'
-                  full="vertical">
-                    <Box direction='row' align='baseline'>
-                        <div className='primary-nav__item'>
-                            <img src="/src/assets/indaba_logo.svg"
-                                className="primary-nav__indaba-logo"/>
+            <nav className='secondary-nav'>
+                        <div className='secondary-nav__left'>
+                            <img src="/src/assets/indaba-logo-white.svg"
+                                className="secondary-nav__indaba-logo"/>
                         </div>
-                    </Box>
-                </Box>
             </nav>
         );
     }
