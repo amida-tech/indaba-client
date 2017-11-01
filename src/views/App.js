040d068cf6403d73722f5edd626d659bdaaed3e4
Lacey Irvin <irvinlacey1988@gmail.com>
2017-11-01 14:09:26 -0400

    added new app.scss file for styling the toast and changed the classsname of the toast to satisfy BEM.

--- src/views/App.js
+++ src/views/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import PrimaryNavContainer from './PrimaryNav';
-
+import SecondaryNavContainer from './SecondaryNav';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className='app'>
                 {this.props.location.pathname === '/login' ?
-                    <PrimaryNavContainer /> :
+                    <SecondaryNavContainer /> :
                     <PrimaryNavContainer /> }
                 <div className='main-body'>
                     {this.props.children}
                 </div>
                 <ToastContainer
-                    className='primary-nav__toast'
+                    className='app__toast'
                     position='top-right'
                     type='default'
                     autoClose={5000}
