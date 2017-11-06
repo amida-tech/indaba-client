5cca484ff8d7800833834f7a0f72c7a205636cea
Christopher Best <bestchris@gmail.com>
2017-11-06 15:06:11 -0500

    load tasks specifically for profile view fix task shape in tasks tab

--- src/views/UserProfile/actions.js
+++ src/views/UserProfile/actions.js
@@ -2,6 +2,8 @@ import * as actionTypes from './actionTypes';
 import * as userActions from '../../common/actions/userActions';
 import * as projectActions from '../../common/actions/projectActions';
 
+import taskService from '../../services/api/tasks';
+
 export const getAllProfileData = (userId, projectId, errorMessages) => (dispatch) => {
     dispatch(_getAllProfileData());
 
@@ -10,8 +12,36 @@ export const getAllProfileData = (userId, projectId, errorMessages) => (dispatch
     if (projectId !== undefined) {
         dispatch(projectActions.getProjectById(projectId));
     }
+
+    dispatch(getTasksForProfile(userId));
+};
+
+export const getTasksForProfile = userId => (dispatch) => {
+    dispatch(_getTasksForProfile());
+
+    taskService.getTasksByUser(userId, (err, response) => {
+        if (err) {
+            dispatch(_getTasksForProfileFailure(err));
+        } else {
+            dispatch(_getTasksForProfileSuccess(response));
+        }
+    });
 };
 
 const _getAllProfileData = () => ({
     type: actionTypes.GET_ALL_PROFILE_DATA,
+});
+
+const _getTasksForProfile = () => ({
+    type: actionTypes.GET_TASKS_FOR_PROFILE,
+});
+
+const _getTasksForProfileFailure = err => ({
+    type: actionTypes.GET_TASKS_FOR_PROFILE_FAILURE,
+    err,
+});
+
+const _getTasksForProfileSuccess = tasks => ({
+    type: actionTypes.GET_TASKS_FOR_PROFILE_SUCCESS,
+    tasks,
 });
