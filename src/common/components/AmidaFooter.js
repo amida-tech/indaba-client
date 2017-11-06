c876553854ed23fa1041aea7275234a456126f55
Lacey Irvin <irvinlacey1988@gmail.com>
2017-11-06 14:48:15 -0500

    created a new watermark and added it the directory and imported title from grommer

--- src/common/components/AmidaFooter.js
+++ src/common/components/AmidaFooter.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Footer } from 'grommet';
+import Title from 'grommet/components/Title';
 
 class AmidaFooter extends Component {
     render() {
         return (
             <Footer justify='between'
                 className='amida-footer'>
-                <p className='amida-footer__footer-title'>
+                <Title className='amida-footer__footer-title'>
                     Powered By Amida
-                </p>
+                </Title>
+                <img src="/src/assets/indaba-SM-white.svg"
+                    className="amida-footer__indaba-service-mark"/>
             </Footer>
         );
     }
