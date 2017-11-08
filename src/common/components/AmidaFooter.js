c59878312da03e31f5d716a9378e9caa1e76635d
Lacey Irvin <irvinlacey1988@gmail.com>
2017-11-08 14:18:46 -0500

    removed the grommet footer and replaced with divs. Also wrapped footer in a div to apply a relative position

--- src/common/components/AmidaFooter.js
+++ src/common/components/AmidaFooter.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { Footer } from 'grommet';
 import Title from 'grommet/components/Title';
 
 class AmidaFooter extends Component {
     render() {
         return (
-            <Footer justify='between'
-                className='amida-footer'>
-                <Title className='amida-footer__footer-title'>
-                    Powered by Amida
-                </Title>
-                <img src="/src/assets/indaba-SM-green.svg"
-                    className="amida-footer__indaba-service-mark"/>
-            </Footer>
+            <div className='amida-footer__container'>
+                    <div className='amida-footer__container-block'>
+                    <Title className='amida-footer__footer-title'>
+                        Powered by Amida
+                    </Title>
+                    <img src="/src/assets/indaba-SM-green.svg"
+                        className="amida-footer__indaba-service-mark"/>
+                </div>
+            </div>
         );
     }
 }
