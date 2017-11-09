a7076eef1d58c06f30bae78cebbfe0f3941658ca
Christopher Best <bestchris@gmail.com>
2017-11-09 12:40:23 -0500

    simplify footer structure, remove Grommet component

--- src/common/components/AmidaFooter.js
+++ src/common/components/AmidaFooter.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import Title from 'grommet/components/Title';
 
 class AmidaFooter extends Component {
     render() {
         return (
-            <div className='amida-footer__container'>
-                    <div className='amida-footer__container-block'>
-                    <Title className='amida-footer__footer-title'>
-                        Powered by Amida
-                    </Title>
-                    <img src="/src/assets/indaba-SM-green.svg"
-                        className="amida-footer__indaba-service-mark"/>
+            <div className='amida-footer'>
+                <div className='amida-footer__footer-title'>
+                    Powered by Amida
                 </div>
+                <img src="/src/assets/indaba-SM-green.svg"
+                    className="amida-footer__indaba-service-mark"/>
             </div>
         );
     }
