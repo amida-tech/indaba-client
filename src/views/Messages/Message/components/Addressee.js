d2b722ccbf443bd6b64a6fe60756938ac560e02c
Christopher Best <bestchris@gmail.com>
2017-11-13 17:01:41 -0500

    restrict addressee remove to icon

--- src/views/Messages/Message/components/Addressee.js
+++ src/views/Messages/Message/components/Addressee.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import IonIcon from 'react-ionicons';
 import { renderName } from '../../../../utils/User';
 
 const Addressee = ({ users, email, onRemove }) => (
-    <div className='addressee'
-        onClick={() => onRemove(email)}>
+    <div className='addressee'>
         {renderName(users.find(user => user.email === email))}
+        <div className='address__delete'
+            onClick={() => onRemove(email)}>
+            <IonIcon className='address__delete-icon'
+                icon='ion-backspace-outline'/>
+        </div>
     </div>
 );
 
