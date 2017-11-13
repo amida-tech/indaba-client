c1e54e9e68ad90dd760be382c1a3489a7cabfb06
Christopher Best <bestchris@gmail.com>
2017-11-13 16:33:25 -0500

    add grommet search to ToField

--- src/views/Messages/Message/components/ToField.js
+++ src/views/Messages/Message/components/ToField.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Search } from 'grommet';
+
+import { renderName } from '../../../../utils/User';
 
 class ToField extends Component {
     render() {
         return (
             <div className='to-field'>
+                <Search
+                    size='small'
+                    value={[]}
+                    suggestions={
+                        this.props.users
+                        .map(user => ({ label: renderName(user),
+                            value: user }))} />
             </div>
         );
     }
 }
 
 ToField.propTypes = {
-    users: PropTypes.arrayOf(PropTypes.object).isRequired,
 
     input: PropTypes.shape({
         value: PropTypes.arrayOf(PropTypes.string).isRequired,
         onChange: PropTypes.func.isRequired,
+        users: PropTypes.arrayOf(PropTypes.object).isRequired,
     }).isRequired,
 };
 
