76e5228f0ff894c13999c8b70fd157287e0eb1e2
jmfadeley <jmfadeley@gmail.com>
2017-11-15 22:32:34 -0500

    Possible fix.

--- src/views/Login/actionTypes.js
+++ src/views/Login/actionTypes.js
@@ -1,6 +1,4 @@
 export const LOGIN = 'LOGIN';
-export const LOGIN_LOADING = 'LOGIN_LOADING';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const CLEAR_LOGIN_FORM = 'CLEAR_LOGIN_FORM';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const GET_CURRENT_USER_FAILURE = 'GET_CURRENT_USER_FAILURE';
