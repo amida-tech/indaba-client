4fac82e8947552e16f315721559ae392d2770033
Christopher Best <bestchris@gmail.com>
2017-11-30 16:36:02 -0500

    add message get actions

--- src/views/PMDashboard/actions.js
+++ src/views/PMDashboard/actions.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import apiService from '../../services/api';
 
 export function setSearchQuery(searchQuery) {
     return {
@@ -11,5 +12,34 @@ export function setFilter(filter) {
     return {
         type: actionTypes.PM_DASH_SET_FILTER,
         filter,
+    };
+}
+
+export function pmGetMessages() {
+    return (dispatch) => {
+        apiService.messaging.list((err, response) => {
+            if (err) {
+                dispatch(_getMessagesFailure());
+            } else {
+                dispatch(_getMessagesSuccess(response));
+            }
+        }, {
+            limit: 4,
+            summary: true,
+        });
+    };
+}
+
+function _getMessagesFailure(err) {
+    return {
+        type: actionTypes.PM_GET_MESSAGES_FAILURE,
+        err,
+    };
+}
+
+function _getMessagesSuccess(messages) {
+    return {
+        type: actionTypes.PM_GET_MESSAGES_SUCCESS,
+        messages,
     };
 }
--- src/views/UserDashboard/actions.js
+++ src/views/UserDashboard/actions.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import apiService from '../../services/api';
 
 export function setSearchQuery(searchQuery) {
     return {
@@ -11,5 +12,34 @@ export function setFilter(filter) {
     return {
         type: actionTypes.USER_DASH_SET_FILTER,
         filter,
+    };
+}
+
+export function userGetMessages() {
+    return (dispatch) => {
+        apiService.messaging.list((err, response) => {
+            if (err) {
+                dispatch(_getMessagesFailure());
+            } else {
+                dispatch(_getMessagesSuccess(response));
+            }
+        }, {
+            limit: 4,
+            summary: true,
+        });
+    };
+}
+
+function _getMessagesFailure(err) {
+    return {
+        type: actionTypes.USER_GET_MESSAGES_FAILURE,
+        err,
+    };
+}
+
+function _getMessagesSuccess(messages) {
+    return {
+        type: actionTypes.USER_GET_MESSAGES_SUCCESS,
+        messages,
     };
 }
