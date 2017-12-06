9980d22bc41a59fcb625f5a478747326e470410d
jmfadeley <jmfadeley@gmail.com>
2017-12-06 14:15:23 -0500

    Changes for handling of the reducer to fix issues.

--- src/common/reducers/userReducer.js
+++ src/common/reducers/userReducer.js
@@ -31,8 +31,6 @@ const initialState = {
 };
 
 export const UserReducer = (state = initialState, action) => {
-    const userIndex = state.users.findIndex(user => user.id === action.userId);
-    // Strongly consider clearing error message here after every call?
     switch (action.type) {
     case type.PUT_PROFILE_SUCCESS:
         return update(state, { profile: { $merge: action.profile } });
@@ -46,10 +44,10 @@ export const UserReducer = (state = initialState, action) => {
         return update(state, { users: { $set: action.users } });
     case type.REPORT_USER_ERROR:
         return update(state, { ui: { errorMessage: { $set: action.error } } });
-    case type.DELETE_USER_SUCCESS:
-        return update(state, {
-            users: { $unset: [userIndex] },
-        });
+    case type.DELETE_USER_SUCCESS: {
+        const userIndex = state.users.findIndex(user => user.id === action.userId);
+        return update(state, { users: { $splice: [[userIndex, 1]] } });
+    }
     case LOG_OUT:
         return initialState;
     default:
