4257a943593c842fbbadb36b6a4b3a12b9404426
Lacey Irvin <irvinlacey1988@gmail.com>
2017-12-06 14:18:00 -0500

    removed the inherited outline from the buttons, changed font inverse color to match new style guide and styled remaining survey pane

--- src/views/TaskReview/components/SurveyPane.js
+++ src/views/TaskReview/components/SurveyPane.js
@@ -1,7 +1,6 @@
  import React, { Component } from 'react';
  import PropTypes from 'prop-types';
  import Select from 'react-select';
- // import { Button } from 'grommet';
  import Time from '../../../utils/Time';
  import SurveyForm from './SurveyForm';
 
