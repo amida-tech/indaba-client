2751427b4bd1de1a03e809cbce4bdb1f097025c7
Christopher Best <bestchris@gmail.com>
2017-12-07 11:00:54 -0500

    add question selecticon list component

---
+++ src/views/ProjectManagement/components/Export/QuestionSelectionList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class QuestionSelectionList extends Component {
+    render() {
+        return (
+            <div className='question-selection-list'>
+                {
+                    this.props.questions.map((question, index) =>
+                    <div key={question.id}
+                        className='question-selection-list__entry'
+                        onClick={() => this.props.onClick(question)}>
+                        <div className='question-selection-list__header'>
+                            {`${this.props.vocab.PROJECT.QUESTION_} ${index + 1}`}
+                        </div>
+                        <div className='question-selection-list__text'>
+                            {question.text}
+                        </div>
+                    </div>)
+                }
+            </div>
+        );
+    }
+}
+
+QuestionSelectionList.propTypes = {
+    questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+    vocab: PropTypes.object.isRequired,
+    onClick: PropTypes.func.isRequired,
+};
+
+export default QuestionSelectionList;
