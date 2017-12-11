2026483f1806954f0e38f54d9c52d3bd5227695f
Lacey Irvin <irvinlacey1988@gmail.com>
2017-12-11 15:36:14 -0500

    imported the react-select into the flagUserSelect, removed block displya, replaced with flex

--- src/views/TaskReview/components/FlagSidebar/FlagUserSelect.js
+++ src/views/TaskReview/components/FlagSidebar/FlagUserSelect.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Select } from 'grommet';
+import Select from 'react-select';
 
 
 class FlagUserSelect extends Component {
