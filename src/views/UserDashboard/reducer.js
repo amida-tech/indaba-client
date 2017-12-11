76f1bb200bd23ce0582415437249b5bafa1f11a4
Christopher Best <bestchris@gmail.com>
2017-12-11 16:31:21 -0500

    add reducer for accepting new data for user dashboard

--- src/views/UserDashboard/reducer.js
+++ src/views/UserDashboard/reducer.js
@@ -1,4 +1,5 @@
 import update from 'immutability-helper';
+import { unionBy } from 'lodash';
 
 import * as actionTypes from './actionTypes';
 import { FILTERS } from './constants';
@@ -9,6 +10,9 @@ const initialState = {
         filter: FILTERS.ALL_TASKS,
     },
     messages: [],
+    tasks: [],
+    answers: [],
+    surveys: [],
 };
 
 export default (state = initialState, action) => {
@@ -25,6 +29,18 @@ export default (state = initialState, action) => {
     case actionTypes.USER_DASH_GET_MESSAGES_SUCCESS:
         return update(state, {
             messages: { $set: action.messages },
+        });
+    case actionTypes.USER_DASH_GET_TASKS_SUCCESS:
+        return update(state, {
+            tasks: { $set: action.tasks },
+        });
+    case actionTypes.USER_DASH_GET_ANSWERS_SUCCESS:
+        return update(state, {
+            answers: answers => unionBy(answers, [action.answers], 'assessmentId'),
+        });
+    case actionTypes.USER_DASH_GET_SURVEY_BY_ID_SUCCESS:
+        return update(state, {
+            surveys: surveys => unionBy(surveys, [action.survey], 'id'),
         });
     default:
         return state;
