a132ec52df4e05d45d76da5e4fc74695b1f1ed98
Christopher Best <bestchris@gmail.com>
2017-12-11 22:16:12 -0500

    prevent stamps from getting too small and wrap gracefully on smaller windows

--- src/views/PMDashboard/components/ProjectGlance.js
+++ src/views/PMDashboard/components/ProjectGlance.js
@@ -7,14 +7,18 @@ class ProjectGlance extends Component {
     render() {
         return (
             <div className='project-glance'>
-                <Stamp label={this.props.vocab.PROJECT.PROJECTS}
-                    value={this.props.projects} />
-                <Stamp label={this.props.vocab.PROJECT.STATUS_ACTIVE}
-                    value={this.props.active} />
-                <Stamp label={this.props.vocab.PROJECT.STATUS_INACTIVE}
-                    value={this.props.inactive} />
-                <Stamp label={this.props.vocab.PROJECT.FLAGS}
-                    value={this.props.flags} />
+                <div className='project-glance__group'>
+                    <Stamp label={this.props.vocab.PROJECT.PROJECTS}
+                        value={this.props.projects} />
+                    <Stamp label={this.props.vocab.PROJECT.STATUS_ACTIVE}
+                        value={this.props.active} />
+                </div>
+                <div className='project-glance__group'>
+                    <Stamp label={this.props.vocab.PROJECT.STATUS_INACTIVE}
+                        value={this.props.inactive} />
+                    <Stamp label={this.props.vocab.PROJECT.FLAGS}
+                        value={this.props.flags} />
+                </div>
             </div>
         );
     }
