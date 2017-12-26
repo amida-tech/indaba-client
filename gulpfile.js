82c339d41198d47e08531fea0f8290d157dbfb34
Elijah George <elijah@amida.com>
2017-12-26 08:59:03 -0500

    refactor/clean-up

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,21 @@
-let gulp = require('gulp');
-let plato = require('es6-plato');
+const gulp = require('gulp');
+const plato = require('es6-plato');
+const lintRules = require('./.eslintrc');
 
-let appSrc = './src/**';
+const appSrc = './src/**';
 
-let outputDir = './artifacts';
+const outputDir = './artifacts';
 
-let lintRules = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import",
-        "react",
-        "babel"
-    ],
-    "rules": {
-        "react/jsx-uses-vars": 1,
-        "react/react-in-jsx-scope": 1,
-        "react/jsx-uses-react": 1,
-        "arrow-body-style": 0,
-        "no-use-before-define": 0,
-        "import/prefer-default-export": 0,
-        "no-underscore-dangle": 0,
-        "class-methods-use-this": 0,
-        "babel/new-cap": 0,
-        "indent": ["error", 4]
-    },
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaFeatures": {
-          "jsx": true
-        }
-    },
-    "env": {
-        "browser": true
-    }
-};
+const complexityRules = {};
 
-
-let complexityRules = {
-
-};
-
-let platoArgs = {
+const platoArgs = {
     title: 'Indaba-Client',
     eslint: lintRules,
-    complexity: complexityRules
+    complexity: complexityRules,
 };
 
 function analysis() {
-  return plato.inspect(appSrc, outputDir, platoArgs);
+    plato.inspect(appSrc, outputDir, platoArgs);
 }
 
 gulp.task('analysis', analysis);
