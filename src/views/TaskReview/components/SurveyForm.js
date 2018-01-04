c021e484cee26749ddcedec178efa5c4615c1b88
jmfadeley <jmfadeley@gmail.com>
2018-01-04 01:16:42 -0500

    Hides the complete buttons if the task is disabled.

--- src/views/TaskReview/components/SurveyForm.js
+++ src/views/TaskReview/components/SurveyForm.js
@@ -24,38 +24,41 @@ class SurveyForm extends Component { // TODO: INBA-450
                         {...this.props} />,
                     )}
                 </Accordion>
-                <div className='survey-form__submit'>
-                    <div className='survey-form__submit-instructions'>
-                        {this.props.vocab.SURVEY.SUBMIT_INSTRUCTIONS}
-                        <br></br>
-                        {(this.props.stage.allowEdit || this.props.stage.discussionParticipation) &&
-                            this.props.vocab.SURVEY.REVIEW_INSTRUCTIONS }
-                        {!this.props.stage.discussionParticipation &&
-                            <div className='survey-form__additional-instructions'>
-                                {this.props.vocab.SURVEY.SUBMIT_INSTRUCTIONS_2}
-                                <Link className='survey-form__link' to='/task'>
-                                    {this.props.vocab.COMMON.MY_TASKS}
-                                </Link>
-                                {this.props.vocab.SURVEY.SUBMIT_INSTRUCTIONS_3}
-                            </div>}
-                    </div>
-                    <button className='survey-form__submit-button'
-                        onClick={() => {
-                            toast(this.props.vocab.PROJECT.TASK_COMPLETED);
-                            this.props.actions.moveTask(
-                                this.props.productId,
-                                this.props.task.uoaId,
-                                this.props.vocab.ERROR,
-                            );
-                        }}>
-                        {this.props.vocab.SURVEY.SUBMIT_TASK}
-                    </button>
-                    {(this.props.stage.allowEdit || this.props.stage.discussionParticipation) &&
+                {!this.props.taskDisabled &&
+                    <div className='survey-form__submit'>
+                        <div className='survey-form__submit-instructions'>
+                            {this.props.vocab.SURVEY.SUBMIT_INSTRUCTIONS}
+                            <br></br>
+                            {(this.props.stage.allowEdit ||
+                                this.props.stage.discussionParticipation) &&
+                                this.props.vocab.SURVEY.REVIEW_INSTRUCTIONS }
+                            {!this.props.stage.discussionParticipation &&
+                                <div className='survey-form__additional-instructions'>
+                                    {this.props.vocab.SURVEY.SUBMIT_INSTRUCTIONS_2}
+                                    <Link className='survey-form__link' to='/task'>
+                                        {this.props.vocab.COMMON.MY_TASKS}
+                                    </Link>
+                                    {this.props.vocab.SURVEY.SUBMIT_INSTRUCTIONS_3}
+                                </div>}
+                        </div>
                         <button className='survey-form__submit-button'
-                            type='submit'>
-                            {this.props.vocab.SURVEY.SAVE_REVIEW}
-                        </button>}
-                </div>
+                            onClick={() => {
+                                toast(this.props.vocab.PROJECT.TASK_COMPLETED);
+                                this.props.actions.moveTask(
+                                    this.props.productId,
+                                    this.props.task.uoaId,
+                                    this.props.vocab.ERROR,
+                                );
+                            }}>
+                            {this.props.vocab.SURVEY.SUBMIT_TASK}
+                        </button>
+                        {(this.props.stage.allowEdit || this.props.stage.discussionParticipation) &&
+                            <button className='survey-form__submit-button'
+                                type='submit'>
+                                {this.props.vocab.SURVEY.SAVE_REVIEW}
+                            </button>}
+                    </div>
+                }
             </form>
         );
     }
