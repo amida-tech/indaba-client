7d6866c1e677f63e96f97650c9dad9a6081a085c
Christopher Best <bestchris@gmail.com>
2018-01-15 14:28:57 -0500

    remove eslint exceptions

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,4 @@
 import devConfig from './development';
 import productionConfig from './production';
 
-let config = devConfig; // eslint-disable-line prefer-const, import/no-mutable-exports
-
-if (process.env.NODE_ENV === 'production') {
-    config = productionConfig;
-}
-
-export default config;
+export default process.env.NODE_ENV === 'production' ? productionConfig : devConfig;
