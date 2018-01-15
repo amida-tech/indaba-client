a686ca434e7207e463202554265e5ae05cf62bee
Christopher Best <bestchris@gmail.com>
2018-01-14 22:59:07 -0500

    specify production NODE_ENV in production webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
@@ -5,5 +6,8 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
     plugins: [
         new UglifyJSPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
     ],
 });
