dcf89ab0b65eb7ae6d07cd73e2c8ecb5f8533f98
Perry Ogwuche <perryogwuche1@gmail.com>
2018-01-16 08:40:30 -0500

    removed log statement

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -7,7 +7,6 @@ const auth = {
     login: (authPayload, callback) => { // Auth does not use cookies for login.
         const path = `${rootURI}/auth/login`;
         const body = `username=${authPayload.username}&password=${authPayload.password}`;
-        console.log(`LOGGING IN WITH: ${body}`);
         requests.apiAuthPostRequest(path, body, callback);
     },
 };
