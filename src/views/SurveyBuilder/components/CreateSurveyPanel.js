4b21a5a5dde63c3c600c0f961c8a12948ce7bb36
Lacey Irvin <irvinlacey1988@gmail.com>
2018-01-17 12:46:45 -0500

    restyled the expand and collapse buttons

--- src/views/SurveyBuilder/components/CreateSurveyPanel.js
+++ src/views/SurveyBuilder/components/CreateSurveyPanel.js
@@ -17,11 +17,11 @@ class CreateSurveyPanel extends Component {
                         disabled={this.props.options.length === 1}
                         onChange={event => this.props.actions.changeSectionView(event.value)}/>
                     <div className='create-survey-panel__accordion-buttons'>
-                        <button className='create-survey-panel__button'
+                        <button className='create-survey-panel__button-expand'
                             onClick={() => toast(this.props.vocab.ERROR.COMING_SOON)}>
                                 {this.props.vocab.PROJECT.EXPAND_ALL}
                             </button>
-                        <button className='create-survey-panel__button'
+                        <button className='create-survey-panel__button-collapse'
                             onClick={() => toast(this.props.vocab.ERROR.COMING_SOON)}>
                                 {this.props.vocab.PROJECT.COLLAPSE_ALL}
                         </button>
