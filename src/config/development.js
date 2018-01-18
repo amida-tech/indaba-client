1a085a3d5625c2be5b7341478ad27321154f3eca
jmfadeley <jmfadeley@gmail.com>
2018-01-18 00:45:11 -0500

    I don't even...

--- src/config/development.js
+++ src/config/development.js
@@ -1,10 +1,12 @@
+const AUTH_API_VERSION = process.env.AUTH_API_VERSION || 'v0';
+
 export default {
     NODE_ENV: 'development',
     API_HTTP_URL: process.env.API_HTTP_URL || 'http://localhost:3005',
     API_HTTPS_URL: process.env.API_HTTPS_URL || 'http://localhost:3005',
 
-    AUTH_API_HTTP_URL: process.env.AUTH_API_HTTP_URL || 'http://localhost:4000/api/v0',
-    AUTH_API_HTTPS_URL: process.env.AUTH_API_HTTPS_URL || 'https://localhost:4000/api/v0',
+    AUTH_API_HTTP_URL: process.env.AUTH_API_HTTP_URL || `http://localhost:4000/api/${AUTH_API_VERSION}`,
+    AUTH_API_HTTPS_URL: process.env.AUTH_API_HTTPS_URL || `https://localhost:4000/api/${AUTH_API_VERSION}`,
 
     SURVEY_API_HTTP_URL: process.env.SURVEY_API_HTTP_URL || 'http://localhost:9005/api/v1.0',
     SURVEY_API_HTTPS_URL: process.env.SURVEY_API_HTTPS_URL || 'https://localhost:9005/api/v1.0',
--- src/config/staging.js
+++ src/config/staging.js
@@ -1,10 +1,12 @@
+const AUTH_API_VERSION = process.env.AUTH_API_VERSION || 'v0';
+
 export default {
     NODE_ENV: 'staging',
     API_HTTP_URL: process.env.API_HTTP_URL || 'http://openshift.amida.com/',
     API_HTTPS_URL: process.env.API_HTTPS_URL || 'https://openshift.amida.com/',
 
-    AUTH_API_HTTP_URL: process.env.AUTH_API_HTTP_URL || 'http://openshift.amida.com/api/v0',
-    AUTH_API_HTTPS_URL: process.env.AUTH_API_HTTPS_URL || 'https://openshift.amida.com/api/v0',
+    AUTH_API_HTTP_URL: process.env.AUTH_API_HTTP_URL || `http://openshift.amida.com/api/${AUTH_API_VERSION}`,
+    AUTH_API_HTTPS_URL: process.env.AUTH_API_HTTPS_URL || `https://openshift.amida.com/api/${AUTH_API_VERSION}`,
     REALM: 'testorg',
 
     SYS_MESSAGE_USER: process.env.SYS_MESSAGE_USER,
