292204e72c3ba935bd7207e8e0770498c2ac764f
Perry Ogwuche <perryogwuche1@gmail.com>
2018-01-23 19:07:06 -0500

    added encodeURIComponent to pass in code to auth service

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -6,7 +6,8 @@ const rootURI = config.AUTH_API_URL;
 const auth = {
     login: (authPayload, callback) => { // Auth does not use cookies for login.
         const path = `${rootURI}/auth/login`;
-        const body = `username=${authPayload.username}&password=${authPayload.password}`;
+        const body = `${encodeURIComponent('username')}=${encodeURIComponent(authPayload.username)}&${
+            encodeURIComponent('password')}=${encodeURIComponent(authPayload.password)}`;
         requests.apiAuthPostRequest(path, body, callback);
     },
 };
