df1f79ad20312c0c28ee38d4e944d0ce8a5ce372
jmfadeley <jmfadeley@gmail.com>
2018-01-29 18:08:43 -0500

    Tweaks to keep discussions within stages.

--- src/views/TaskReview/components/FlagSidebar/FlagHeader.js
+++ src/views/TaskReview/components/FlagSidebar/FlagHeader.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { every, sumBy } from 'lodash';
 
 class FlagHeader extends Component {
     render() {
+        const flagCount = sumBy(this.props.ui.flags, (flag) => {
+            return every(flag.discussion, discuss => discuss.isResolve) ? 0 : 1;
+        });
         return (
             <div className='flag-header'>
                 <span className='flag-header__title'>
                     {this.props.vocab.PROJECT.FLAGS}
                 </span>
                 <div className='flag-header__info'>
-                    {this.props.ui.flags.length}{this.props.vocab.PROJECT._FLAGS_REPORTED}
+                    {flagCount}{this.props.vocab.PROJECT._FLAGS_REPORTED}
                 </div>
             </div>
         );
