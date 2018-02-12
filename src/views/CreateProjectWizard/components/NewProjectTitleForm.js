f02c672d22614cb17fc83b77584dfb4d8a384589
Christopher Best <bestchris@gmail.com>
2018-02-12 12:22:30 -0500

    fix class names to align with components

--- src/views/CreateProjectWizard/components/NewProjectTitleForm.js
+++ src/views/CreateProjectWizard/components/NewProjectTitleForm.js
@@ -7,22 +7,22 @@ class NewProjectTitleForm extends Component {
     render() {
         return (
             <form onSubmit={this.props.handleSubmit}
-                className='new-project-title'>
+                className='new-project-title-form'>
                 <Field component={ValidatedTextInput}
                     name='project.codeName'
-                    className='new-project-title__name'
+                    className='new-project-title-form__name'
                     placeholder={this.props.vocab.PROJECT.TITLE}
                     validate={value => (!value ?
                         this.props.vocab.VALIDATE.FIELD_REQUIRED :
                         undefined)}/>
-                <div className='new-project-title__summary-container'>
+                <div className='new-project-title-form__summary-container'>
                     <Field component='textarea'
                         name='project.description'
                         placeholder={this.props.vocab.PROJECT.SUMMARY}
-                        className='new-project-title__summary' />
+                        className='new-project-title-form__summary' />
                 </div>
                 {this.props.errorMessage &&
-                    <div className='new-project-title__error'>
+                    <div className='new-project-title-form__error'>
                         {this.props.errorMessage}
                     </div>
                 }
