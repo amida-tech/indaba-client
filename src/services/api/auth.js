be4ba6a7a56ddc6f65ee19f0f4cd80f528190825
Christopher Best <bestchris@gmail.com>
2018-02-14 11:13:13 -0500

    raise promises to login actions and remove callback

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -4,7 +4,7 @@ import config from '../../config';
 const rootURI = config.AUTH_API_URL;
 
 const auth = {
-    login: (authPayload, callback) => { // Auth does not use cookies for login.
+    login: (authPayload) => { // Auth does not use cookies for login.
         const path = `${rootURI}/auth/login`;
 
         const body = {
@@ -12,14 +12,12 @@ const auth = {
             password: authPayload.password,
         };
 
-        requests.apiAuthPostRequest(path, body)
-        .then(response => callback(null, response), err => callback(err));
+        return requests.apiAuthPostRequest(path, body);
     },
-    requestResetToken: (email, callback) => {
+    requestResetToken: (email) => {
         const path = `${rootURI}/auth/reset-password`;
         const body = { email };
-        requests.apiAuthPostRequest(path, body)
-        .then(response => callback(null, response), err => callback(err));
+        return requests.apiAuthPostRequest(path, body);
     },
     resetPassword: (token, password) => {
         const path = `${rootURI}/auth/reset-password/${token}`;
