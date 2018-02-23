42331432fa16b3b95a6c0fc045784d1fb893f325
Lacey Irvin <irvinlacey1988@gmail.com>
2018-02-22 22:56:34 -0500

    changed grommt picker to infinite

--- src/views/ProjectManagement/components/Modals/Stage/StageDateTime.js
+++ src/views/ProjectManagement/components/Modals/Stage/StageDateTime.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import DateTime from 'grommet/components/DateTime';
+import DateInput from '../../../../../common/components/DateInput';
 
 class StageDateTime extends Component {
     render() {
         return (
-            <DateTime
+            <DateInput
                 {...this.props.input}
                 format='MM/DD/YYYY'
                 className='stage-date-time' />
