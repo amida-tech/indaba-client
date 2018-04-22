b88e25e21984fe8a91acb0050974f41c3ba94c96
Unprofessional <unprofessionalmadman@gmail.com>
2018-04-22 00:47:15 -0400

    Clean up code and add note for future work

--- src/views/ProjectManagement/components/Modals/Stage/StageDateTime.js
+++ src/views/ProjectManagement/components/Modals/Stage/StageDateTime.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import DateInput from '../../../../../common/components/DateInput';
 
 class StageDateTime extends Component {
+
+    /* TODO: Need to find a way to add this here to reflect endOfDay
+        onChange={(event) => {
+            const endOfDay = new Date(event).setHours(23, 59, 59, 999);
+            this.props.actions.updateTask(
+                this.props.task.id,
+                this.props.task.userIds,
+                new Date(endOfDay),
+                this.props.vocab.ERROR);
+        }}
+    */
+
     render() {
         return (
             <DateInput
