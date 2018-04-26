6ddc894c574f5f4ba438e27105355ed3cd5d2b5d
jmfadeley <jmfadeley@gmail.com>
2018-04-26 18:07:03 -0400

    Changes that *seem* to handle discrepancies in time zone for whether a task is due today or tomorrow.

--- src/views/ProjectManagement/components/Workflow/StageSlot.js
+++ src/views/ProjectManagement/components/Workflow/StageSlot.js
@@ -84,8 +84,6 @@ class StageSlot extends Component {
     }
 
     displayStatus(done, diff) {
-        // console.log('displayStatus');
-        // console.log(diff);
         if (done) {
             return { label: this.props.vocab.PROJECT.CARD.DONE, type: StatusLabelType.GOOD };
         }
