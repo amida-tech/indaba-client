1f146467516ce038387fb1511b11e1fd7f8c14b7
jmfadeley <jmfadeley@gmail.com>
2018-07-10 16:58:37 -0400

    Minor update to .env

--- src/services/api/messaging.js
+++ src/services/api/messaging.js
@@ -1,7 +1,7 @@
 import * as requests from './requests';
 import config from '../../config';
 
-const rootURI = config.MESSAGING_API_URL;
+const rootURI = config.MESSAGING_MICROSERVICE_URL;
 
 const messaging = {
     send: (message) => {
