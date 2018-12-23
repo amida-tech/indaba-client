3487a9181daadc1bc7a745ac5f52198fea69f7be
Unprofessional <unprofessionalmadman@gmail.com>
2018-12-23 15:07:58 -0500

    Address the build warnings about export 'default' not found in the action exporters

--- src/views/ProjectManagement/index.js
+++ src/views/ProjectManagement/index.js
@@ -1,3 +1,3 @@
-export { default as ProjectManagementActions } from './actions';
+export * as ProjectManagementActions from './actions';
 export { default as ProjectManagementContainer } from './components';
 export { default as ProjectManagementReducer } from './reducer';
--- src/views/TaskReview/index.js
+++ src/views/TaskReview/index.js
@@ -1,3 +1,3 @@
-export { default as TaskReviewActions } from './actions';
+export * as TaskReviewActions from './actions';
 export { default as TaskReview } from './components';
 export { default as TaskReviewReducer } from './reducer';
