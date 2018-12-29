f314d566942c00eaab8b9713008a4a60e20df7ef
jmfadeley@gmail.com <jmfadeley@gmail.com>
2018-12-29 00:37:21 -0500

    [INBA-950] This method is better.

--- src/config/production.js
+++ src/config/production.js
@@ -1,17 +1,13 @@
-import cookie from 'react-cookies';
-import { get } from 'lodash';
-
-const settings = cookie.load('indabaConfig');
-
+console.log(window.INDABA_CLIENT_URL);
 export default {
     NODE_ENV: 'production',
-    INDABA_CLIENT_URL: get(settings, 'INDABA_CLIENT_URL') || process.env.INDABA_CLIENT_URL,
-    GREYSCALE_URL: get(settings, 'GREYSCALE_URL') || process.env.GREYSCALE_URL,
-    AUTH_MICROSERVICE_URL: get(settings, 'AUTH_MICROSERVICE_URL') || process.env.AUTH_MICROSERVICE_URL,
-    SURVEY_MICROSERVICE_URL: get(settings, 'SURVEY_MICROSERVICE_URL') || process.env.SURVEY_MICROSERVICE_URL,
-    MESSAGING_MICROSERVICE_URL: get(settings, 'MESSAGING_MICROSERVICE_URL')
+    INDABA_CLIENT_URL: window.INDABA_CLIENT_URL || process.env.INDABA_CLIENT_URL,
+    GREYSCALE_URL: window.GREYSCALE_URL || process.env.GREYSCALE_URL,
+    AUTH_MICROSERVICE_URL: window.AUTH_MICROSERVICE_URL || process.env.AUTH_MICROSERVICE_URL,
+    SURVEY_MICROSERVICE_URL: window.SURVEY_MICROSERVICE_URL || process.env.SURVEY_MICROSERVICE_URL,
+    MESSAGING_MICROSERVICE_URL: window.MESSAGING_MICROSERVICE_URL
         || process.env.MESSAGING_MICROSERVICE_URL,
-    INDABA_CLIENT_DEFAULT_REALM: get(settings, 'INDABA_CLIENT_DEFAULT_REALM')
+    INDABA_CLIENT_DEFAULT_REALM: window.INDABA_CLIENT_DEFAULT_REALM
         || process.env.INDABA_CLIENT_DEFAULT_REALM,
-    SYS_MESSAGE_USER: get(settings, 'SYS_MESSAGE_USER') || process.env.SYS_MESSAGE_USER,
+    SYS_MESSAGE_USER: window.SYS_MESSAGE_USER || process.env.SYS_MESSAGE_USER,
 };
