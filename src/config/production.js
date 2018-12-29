1de6e1c15b4963c3d9dbe0d34e0462fdac9e408a
jmfadeley@gmail.com <jmfadeley@gmail.com>
2018-12-28 22:58:46 -0500

    [INBA-950] Fixes for deployments. Undefined thrown because of a check loading production, even if it weren't in production mode.

--- src/config/production.js
+++ src/config/production.js
@@ -1,15 +1,17 @@
 import cookie from 'react-cookies';
+import { get } from 'lodash';
 
 const settings = cookie.load('indabaConfig');
+
 export default {
     NODE_ENV: 'production',
-    INDABA_CLIENT_URL: settings.INDABA_CLIENT_URL || process.env.INDABA_CLIENT_URL,
-    GREYSCALE_URL: settings.GREYSCALE_URL || process.env.GREYSCALE_URL,
-    AUTH_MICROSERVICE_URL: settings.AUTH_MICROSERVICE_URL || process.env.AUTH_MICROSERVICE_URL,
-    SURVEY_MICROSERVICE_URL: settings.SURVEY_MICROSERVICE_URL || process.env.SURVEY_MICROSERVICE_URL,
-    MESSAGING_MICROSERVICE_URL: settings.MESSAGING_MICROSERVICE_URL
+    INDABA_CLIENT_URL: get(settings, 'INDABA_CLIENT_URL') || process.env.INDABA_CLIENT_URL,
+    GREYSCALE_URL: get(settings, 'GREYSCALE_URL') || process.env.GREYSCALE_URL,
+    AUTH_MICROSERVICE_URL: get(settings, 'AUTH_MICROSERVICE_URL') || process.env.AUTH_MICROSERVICE_URL,
+    SURVEY_MICROSERVICE_URL: get(settings, 'SURVEY_MICROSERVICE_URL') || process.env.SURVEY_MICROSERVICE_URL,
+    MESSAGING_MICROSERVICE_URL: get(settings, 'MESSAGING_MICROSERVICE_URL')
         || process.env.MESSAGING_MICROSERVICE_URL,
-    INDABA_CLIENT_DEFAULT_REALM: settings.INDABA_CLIENT_DEFAULT_REALM
+    INDABA_CLIENT_DEFAULT_REALM: get(settings, 'INDABA_CLIENT_DEFAULT_REALM')
         || process.env.INDABA_CLIENT_DEFAULT_REALM,
-    SYS_MESSAGE_USER: settings.SYS_MESSAGE_USER || process.env.SYS_MESSAGE_USER,
+    SYS_MESSAGE_USER: get(settings, 'SYS_MESSAGE_USER') || process.env.SYS_MESSAGE_USER,
 };
