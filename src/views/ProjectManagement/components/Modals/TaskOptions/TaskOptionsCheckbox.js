c16f3465929b883418d1fab764cab99194c0d5c2
Eric West <eric.west@amida.com>
2019-01-08 21:05:44 -0500

    code functionality achieved. Styling still needs work.

--- src/views/ProjectManagement/components/Modals/TaskOptions/TaskOptionsCheckbox.js
+++ src/views/ProjectManagement/components/Modals/TaskOptions/TaskOptionsCheckbox.js
@@ -4,13 +4,33 @@ import CheckBox from 'grommet/components/CheckBox';
 class TaskOptionsCheckbox extends Component {
     render() {
         return (
-            <CheckBox
-                checked={!!this.props.input.value}
-                onChange={this.props.input.onChange}
-                className='task-options-form__header'
-                label={this.props.label}
-                disabled={this.props.disabled}
-            />
+            // <CheckBox
+            //     checked={!!this.props.input.value}
+            //     onChange={this.props.input.onChange}
+            //     className='task-options-form__header'
+            //     label={this.props.label}
+            //     disabled={this.props.disabled}
+            // />
+            <label htmlFor={this.props.input.value}>
+                <input type={'checkbox'}
+                    id={this.props.input.value}
+                    checked={typeof this.props.input.value === 'boolean'
+                        ? this.props.input.value : false}
+                    label= {this.props.label}
+                    onChange={this.props.input.onChange}
+                    disabled={this.props.disabled}
+                    className='task-options-form__checkbox-hidden'
+                />
+                <span
+                    id={this.props.input.value}
+                    className={`
+                            far fa-${this.props.input.checked ? 'check-' : ''}square
+                            task-options-form__checkbox
+                            task-options-form__checkbox-${this.props.input.checked ? 'checked' : 'unchecked'}
+                        `}
+                />
+                {this.props.label}
+            </label>
         );
     }
 }
