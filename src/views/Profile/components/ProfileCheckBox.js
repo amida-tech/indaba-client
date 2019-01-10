35576e82e95ca61e2d7d14fe7a0371ef7deebb02
Eric West <eric.west@amida.com>
2019-01-10 15:51:33 -0500

    removed redundant form tag, and empty value and bad spacing in CSS that were preventing compiling from yarn develop

--- src/views/Profile/components/ProfileCheckBox.js
+++ src/views/Profile/components/ProfileCheckBox.js
@@ -4,25 +4,23 @@ class ProfileCheckBox extends Component {
     render() {
         return (
             <div>
-                <form>
-                    <input type="checkbox"
-                        id={this.props.input.value}
-                        checked={typeof this.props.input.value === 'boolean'
-                            ? this.props.input.value : false}
-                        value={this.props.input.value}
-                        name={this.props.input.value}
-                        onChange={this.props.input.onChange}
-                        disabled={true}
-                        className='profile-form__checkbox--hidden'
-                    />
-                    <label
-                        for={this.props.input.value}
-                        className={`
+                <input type="checkbox"
+                    id={this.props.input.value}
+                    checked={typeof this.props.input.value === 'boolean'
+                        ? this.props.input.value : false}
+                    value={this.props.input.value}
+                    name={this.props.input.value}
+                    onChange={this.props.input.onChange}
+                    disabled={true}
+                    className='profile-form__checkbox--hidden'
+                />
+                <label
+                    for={this.props.input.value}
+                    className={`
                             far fa-${this.props.input.checked ? 'check-' : ''}square
                             profile-form__checkbox--disabled
                         `}
-                    />
-                </form>
+                />
             </div>
         );
     }
