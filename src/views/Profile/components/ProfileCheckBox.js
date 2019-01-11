3d014319433b6b8411b7214d1f45114a810b0d51
Eric West <eric.west@amida.com>
2019-01-11 12:56:04 -0500

    Checkbox functionality achieved and styling match achieved.

--- src/views/Profile/components/ProfileCheckBox.js
+++ src/views/Profile/components/ProfileCheckBox.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import CheckBox from 'grommet/components/CheckBox';
 
 class ProfileCheckBox extends Component {
     render() {
         return (
-            <CheckBox
-                checked={typeof this.props.input.value === 'boolean'
-                    ? this.props.input.value : false }
-                value={this.props.input.value}
-                onChange={this.props.input.onChange}
-                disabled={true}
-                className='update-profile-form__checkbox' />
+            <div>
+                <input type="checkbox"
+                    id={this.props.input.value}
+                    checked={typeof this.props.input.value === 'boolean'
+                        ? this.props.input.value : false}
+                    value={this.props.input.value}
+                    name={this.props.input.value}
+                    onChange={this.props.input.onChange}
+                    disabled={true}
+                    className='profile-form__checkbox--hidden'
+                />
+                <label
+                    for={this.props.input.value}
+                    className={`
+                            far fa-${this.props.input.checked ? 'check-' : ''}square
+                            profile-form__checkbox--disabled
+                        `}
+                />
+            </div>
         );
     }
 }
