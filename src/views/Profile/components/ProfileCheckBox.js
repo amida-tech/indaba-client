8a3f70ee7f8c815ce923bc2bc5c4486ef330af8e
Eric West <eric.west@amida.com>
2019-01-14 10:55:56 -0500

    Fixed console errors in profile, updated code between taskoptions and profile to have similar formatting.

--- src/views/Profile/components/ProfileCheckBox.js
+++ src/views/Profile/components/ProfileCheckBox.js
@@ -3,25 +3,25 @@ import React, { Component } from 'react';
 class ProfileCheckBox extends Component {
     render() {
         return (
-            <div>
-                <input type="checkbox"
-                    id={this.props.input.value}
+            <label
+                htmlFor={this.props.input.name}
+            >
+                <input type={'checkbox'}
+                    id={this.props.input.name}
                     checked={typeof this.props.input.value === 'boolean'
                         ? this.props.input.value : false}
-                    value={this.props.input.value}
-                    name={this.props.input.value}
                     onChange={this.props.input.onChange}
                     disabled={true}
                     className='profile-form__checkbox--hidden'
                 />
-                <label
-                    for={this.props.input.value}
+                <span
+                    id={this.props.input.name}
                     className={`
-                            far fa-${this.props.input.checked ? 'check-' : ''}square
-                            profile-form__checkbox--disabled
-                        `}
+                        far fa-${this.props.input.checked ? 'check-' : ''}square
+                        profile-form__checkbox--disabled
+                    `}
                 />
-            </div>
+            </label>
         );
     }
 }
