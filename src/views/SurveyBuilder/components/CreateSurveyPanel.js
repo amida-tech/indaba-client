95611b0feb2cd3d284b0a7bdb852df17a3a1be8a
Riley Orr <riley@amida.com>
2019-01-17 16:44:23 -0500

    removed expand & collapse button functionality and styling for survey builder

--- src/views/SurveyBuilder/components/CreateSurveyPanel.js
+++ src/views/SurveyBuilder/components/CreateSurveyPanel.js
@@ -29,16 +29,6 @@ class CreateSurveyPanel extends Component {
                         clearable={false}
                         disabled={this.props.options.length === 1}
                         onChange={event => this.props.actions.changeSectionView(event.value)}/>
-                    <div className='create-survey-panel__accordion-buttons'>
-                        <button className='create-survey-panel__button-expand'
-                            onClick={() => toast(this.props.vocab.ERROR.COMING_SOON)}>
-                            {this.props.vocab.PROJECT.EXPAND_ALL}
-                        </button>
-                        <button className='create-survey-panel__button-collapse'
-                            onClick={() => toast(this.props.vocab.ERROR.COMING_SOON)}>
-                            {this.props.vocab.PROJECT.COLLAPSE_ALL}
-                        </button>
-                    </div>
                 </div>
                 <div className='create-survey-panel__survey-controls'>
                     <button className='create-survey-panel__survey-save'
