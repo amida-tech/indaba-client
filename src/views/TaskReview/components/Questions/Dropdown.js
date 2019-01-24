73139bc0d20318bd391489822e8bc1434cbfda34
jmfadeley@gmail.com <jmfadeley@gmail.com>
2019-01-24 14:13:48 -0500

    [INBA-977] Fixes for the dropdown.

--- src/views/TaskReview/components/Questions/Dropdown.js
+++ src/views/TaskReview/components/Questions/Dropdown.js
@@ -1,35 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
 import Select from 'react-select';
 
 class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options: this.props.choices.map((entry) => {
+                return { label: entry.text, value: entry.id };
+            })
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt) {
+        this.props.upsertAnswer({ choice: evt.value });
+    }
+
     render() {
         let currentValue = get(this.props, 'answer', undefined);
         if (currentValue) {
-            currentValue = this.props.choices.find(choice => choice.id === currentValue.choice).text;
-        }
+            currentValue = {
+                value: currentValue,
+                label: this.props.choices.find(choice => choice.id === currentValue.choice).text,
+            }
+        };
         return (
             <div className='dropdown'>
                 {
                     this.props.displayMode
                         ? <select className='dropdown__field-disabled'
-                            value={currentValue}
+                            value={currentValue.label}
                             placeholder={this.props.vocab.PROJECT.SELECT_OPTION}
                             disabled={true}>
                             <option className='dropdown__option'
-                                label={currentValue} />
+                                label={currentValue.label} />
                         </select>
                         : <Select className='dropdown__field'
                             value={currentValue}
                             placeHolder={this.props.vocab.PROJECT.SELECT_OPTION}
                             readOnly={this.props.displayMode}
-                            options={this.props.choices.map((entry) => {
-                                return { label: entry.text, value: entry.id };
-                            })}
-                            onChange={(event) => {
-                                this.props.upsertAnswer({ choice: event.option.value });
-                            }} />
+                            options={this.state.options}
+                            clearable={false}
+                            onChange={this.handleChange} />
                 }
             </div>
         );
