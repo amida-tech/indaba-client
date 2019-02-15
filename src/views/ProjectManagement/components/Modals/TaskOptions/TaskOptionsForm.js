01cdf8232aea2e9e83b7f3f4ad131f56e969862f
Eric West <eric.west@amida.com>
2019-02-15 13:51:15 -0500

    updated DOM and language file for new functionality.

--- src/views/ProjectManagement/components/Modals/TaskOptions/TaskOptionsForm.js
+++ src/views/ProjectManagement/components/Modals/TaskOptions/TaskOptionsForm.js
@@ -10,6 +10,7 @@ import { renderName } from '../../../../../utils/User';
 class TaskOptionsForm extends Component {
     render() {
         const disabled = this.props.task.status === 'completed';
+        console.log(this.props.userOptions);
         return (
             <div className={`task-options-form__container${disabled ? ' task-options-form__container--disabled' : ''}`}>
                 <form className='task-options-form'
@@ -22,8 +23,20 @@ class TaskOptionsForm extends Component {
                         label={this.props.vocab.PROJECT.OPTIONS_MODAL.FORCE}
                         disabled={disabled}
                     />
+
                     <div className='task-options-form__header-paragraph'>
                         {this.props.vocab.PROJECT.OPTIONS_MODAL.FORCE_PARAGRAPH}
+                    </div>
+                    <Field
+                        name='choice'
+                        component={TaskOptionsRadio}
+                        type='radio'
+                        value='unassign'
+                        label={this.props.vocab.PROJECT.OPTIONS_MODAL.UNASSIGN}
+                        disabled={disabled}
+                    />
+                    <div className='task-options-form__header-paragraph'>
+                        {this.props.vocab.PROJECT.OPTIONS_MODAL.UNASSIGN_PARAGRAPH}
                     </div>
                     <Field
                         name='choice'
@@ -41,7 +54,7 @@ class TaskOptionsForm extends Component {
                         userOptions={this.props.userOptions}
                         disabled={disabled}
                     />
-                    <hr className='task-options-form__divider'/>
+                    <hr className='task-options-form__divider' />
                     <Field
                         name='notify'
                         component={TaskOptionsCheckbox}
@@ -51,13 +64,13 @@ class TaskOptionsForm extends Component {
                         disabled={disabled}
                     />
                     <div className='task-options__notify-user-warning'>
-                        { renderName(this.props.currentUser)
-                                + this.props.vocab.PROJECT.OPTIONS_MODAL._WILL_BE_NOTIFIED }
+                        {renderName(this.props.currentUser)
+                            + this.props.vocab.PROJECT.OPTIONS_MODAL._WILL_BE_NOTIFIED}
                     </div>
                     <Field
                         name='message'
                         className={'task-options-form__header-text-box'
-                                + ' task-options-form__notify-user-warning-text-box'}
+                            + ' task-options-form__notify-user-warning-text-box'}
                         component='textarea'
                         disabled={disabled}
                     />
