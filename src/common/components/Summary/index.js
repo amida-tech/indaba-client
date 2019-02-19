e1a6d61b476fa9ca0b80d4a83e34abe1eae48332
morgan peters <morgan@amida.com>
2019-02-19 12:29:02 -0500

    modal footer and colors

--- src/common/components/Summary/index.js
+++ src/common/components/Summary/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import IonIcon from 'react-ionicons';
 import { toast } from 'react-toastify';
 import { has, some } from 'lodash';
 
