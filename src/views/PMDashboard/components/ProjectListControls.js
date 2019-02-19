2d738404c3440f96f2dd98d18a707cfc645d86c3
morgan peters <morgan@amida.com>
2019-02-19 14:26:29 -0500

    removed filter, updated messages, login form

--- src/views/PMDashboard/components/ProjectListControls.js
+++ src/views/PMDashboard/components/ProjectListControls.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import ProjectFilter from './ProjectFilter';
-import FilterInput from '../../../common/components/Dashboard/FilterInput';
 
 class ProjectListControls extends Component {
     render() {
         return (
             <div className='project-list-controls'>
-                <div className='project-list-controls__search'>
-                    <FilterInput
-                        placeholder={this.props.vocab.PROJECT.FILTER_PROJECTS}
-                        onChange={evt => this.props.actions.setFilterQuery(evt.target.value)} />
-                </div>
                 <div className='project-list-controls__filter'>
                     <ProjectFilter active={this.props.filter}
                         vocab={this.props.vocab}
