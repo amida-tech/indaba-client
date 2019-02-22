e689503e28b1860e7afe244bb99c1189c3ec44ab
jmfadeley@gmail.com <jmfadeley@gmail.com>
2019-02-22 13:57:32 -0500

    Changes for reset password.

--- src/views/ResetPassword/components/ResetPasswordForm.js
+++ src/views/ResetPassword/components/ResetPasswordForm.js
@@ -1,24 +1,84 @@
 import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
-
-import { required } from '../../../common/validation';
-import ValidatedTextInput from '../../../common/components/ValidatedTextInput';
 
 class ResetPasswordForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            password: '',
+            confirmPassword: '',
+            passwordFlag: false,
+            confirmPasswordFlag: false,
+        };
+
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleValidate = this.handleValidate.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handlePassword(evt) {
+        if (evt.target.name === 'password') {
+            this.setState({ password: evt.target.value });
+        } else {
+            this.setState({ confirmPassword: evt.target.value });
+        }
+    }
+
+    handleValidate(evt) {
+        if (!evt) { // User hit enter. Check everything because evt is synth.
+            const passwordMismatch = this.state.password !== this.state.confirmPassword;
+            const passwordFlag = this.state.password === '';
+            const confirmPasswordFlag = this.state.confirmPassword === '';
+            this.setState({
+                passwordFlag,
+                confirmPasswordFlag,
+            });
+            if (passwordMismatch) {
+                this.props.actions.resetPasswordUIMessage(
+                    this.props.vocab.RESET_PASSWORD.PASSWORD_MATCH,
+                    true,
+                );
+            }
+            return passwordFlag || confirmPasswordFlag || passwordMismatch;
+        }
+        if (evt.target.name === 'password') {
+            this.setState({ passwordFlag: evt.target.value === '' });
+        } else {
+            this.setState({ confirmPasswordFlag: evt.target.value === '' });
+        }
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+        if (this.handleValidate()) {
+            return;
+        }
+        this.props.onSubmit(this.state.password);
+    }
+
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit}
+            <form onSubmit={this.handleSubmit}
                 className='reset-password-form'>
                 <div className='reset-password-form__instructions'>
                     {this.props.vocab.RESET_PASSWORD.INSTRUCTIONS}
                 </div>
-                <Field component={ValidatedTextInput}
-                    className='reset-password-form__password'
-                    password={true}
+                <input className={`reset-password-form__input ${this.state.passwordFlag
+                    ? 'reset-password-form__input--flag' : ''}`}
                     name='password'
-                    placeholder={this.props.vocab.RESET_PASSWORD.NEW_PASSWORD}
-                    vocab={this.props.vocab.VALIDATE}
-                    validate={[required]}/>
+                    placeholder={this.state.passwordFlag
+                        ? this.props.vocab.RESET_PASSWORD.NEW_PASSWORD_REQUIRED
+                        : this.props.vocab.RESET_PASSWORD.NEW_PASSWORD}
+                    onBlur={this.handleValidate}
+                    onChange={this.handlePassword}/>
+                <input className={`reset-password-form__input ${this.state.confirmPasswordFlag
+                    ? 'reset-password-form__input--flag' : ''}`}
+                    name='confirmPassword'
+                    placeholder={this.state.confirmPasswordFlag
+                        ? this.props.vocab.RESET_PASSWORD.CONFIRM_PASSWORD_REQUIRED
+                        : this.props.vocab.RESET_PASSWORD.CONFIRM_PASSWORD}
+                    onBlur={this.handleValidate}
+                    onChange={this.handlePassword}/>
                 <button className='reset-password-form__button'
                     type='submit'>
                     {this.props.vocab.RESET_PASSWORD.LINK}
@@ -28,4 +88,4 @@ class ResetPasswordForm extends Component {
     }
 }
 
-export default reduxForm({ form: 'reset-password-form' })(ResetPasswordForm);
+export default ResetPasswordForm;
